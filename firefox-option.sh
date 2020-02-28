dc74fe033fa5bade0dc596f1a1c8f1238d4ad8a8
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-02-28 00:40:53 -0500

    initial release of collective Firefox auto install
    The initial release of the collective Firefox automated installation process.  This file as only a bonus and was designed in mind for people who would have installed all editions (primarily developers) and as a means of saving them time.  The core goal of this development is to make installing native Mozilla Firefox easier and quicker.

---
+++ firefox-option.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# This will install ALL editions of Mozilla Firefox
+# This is only an option.
+#
+# Firefox Stable Edition  ( FirefoxStable.sh )
+# Firefox Beta ( FirefoxBeta.sh )
+# Firefox Developer Edition ( Firefox-Developer-Edition.sh )
+# Firefox Nightly ( FirefoxNightly.sh )
+#
+#
+echo
+chmod +x FirefoxStable.sh FirefoxBeta.sh Firefox-Developer-Edition.sh FirefoxNightly.sh
+echo
+ while true; do
+    read -p "This will install ALL editions of Mozilla Firefox onto your computer.
+
+Firefox
+Firefox Beta
+Firefox Developer Edition
+Firefox Nightly
+
+This is only an OPTION and you are not required to install ALL editions.
+
+If you intended to only install, for example, 
+the latest stable version of Firefox you can simply run FirefoxStable.sh 
+
+Are you sure wish to install ALL editions of Mozilla Firefox 
+onto your computer?" yn
+    case $yn in
+        [Yy]* ) ./FirefoxStable.sh; ./FirefoxBeta.sh; ./Firefox-Developer-Edition.sh; ./FirefoxNightly.sh; echo; echo; echo ALL editions of Mozilla Firfox have been installed; echo  They ALL will update themselves. No additional action is required.; echo; echo Happy Browsing!; echo; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+exit 0
