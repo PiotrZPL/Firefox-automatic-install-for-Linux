c33ed4f7ba74b43786ee2ce632b9e63fda4df8f6
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-03-11 18:11:32 -0400

    extended to include FirefoxESR
    Extended to include Firefox Extended Support Release (Firefox ESR).

--- firefox-option.sh
+++ firefox-option.sh
@@ -7,10 +7,11 @@
 # Firefox Beta ( FirefoxBeta.sh )
 # Firefox Developer Edition ( Firefox-Developer-Edition.sh )
 # Firefox Nightly ( FirefoxNightly.sh )
+# Firefox Extended Support Release ( FirefoxESR.sh )
 #
 #
 echo
-chmod +x FirefoxStable.sh FirefoxBeta.sh Firefox-Developer-Edition.sh FirefoxNightly.sh
+chmod +x FirefoxStable.sh FirefoxBeta.sh Firefox-Developer-Edition.sh FirefoxNightly.sh FirefoxESR.sh
 echo
  while true; do
     read -p "This will install ALL editions of Mozilla Firefox onto your computer.
@@ -19,6 +20,7 @@ Firefox
 Firefox Beta
 Firefox Developer Edition
 Firefox Nightly
+Firefox Extended Support Release
 
 This is only an OPTION and you are not required to install ALL editions.
 
@@ -28,7 +30,7 @@ the latest stable version of Firefox you can simply run FirefoxStable.sh
 Are you sure wish to install ALL editions of Mozilla Firefox 
 onto your computer?" yn
     case $yn in
-        [Yy]* ) ./FirefoxStable.sh; ./FirefoxBeta.sh; ./Firefox-Developer-Edition.sh; ./FirefoxNightly.sh; echo; echo; echo ALL editions of Mozilla Firfox have been installed; echo  They ALL will update themselves. No additional action is required.; echo; echo Happy Browsing!; echo; break;;
+        [Yy]* ) ./FirefoxStable.sh; ./FirefoxBeta.sh; ./Firefox-Developer-Edition.sh; ./FirefoxNightly.sh; ./FirefoxESR.sh; echo; echo; echo ALL editions of Mozilla Firfox have been installed; echo  They ALL will update themselves. No additional action is required.; echo; echo Happy Browsing!; echo; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
