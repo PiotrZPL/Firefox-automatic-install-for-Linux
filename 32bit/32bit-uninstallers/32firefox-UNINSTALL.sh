6b9e1f8a654d8e3e4a2b2dcdddaa701659351ba7
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-03-11 21:32:57 -0400

    Initial release Firefox 32-bit uninstaller
    The first release of the uninstaller for Firefox (stable) 32-bit uninstaller.

---
+++ 32bit/32bit-uninstallers/32firefox-UNINSTALL.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# That which is done. Cannot be undone. Reinstalled, of course! But not undone.
+#
+echo
+sudo rm -r -f  /opt/firefox-32bit/
+#
+# As noted in dev-doc.txt if you are a developer or enthusiast 
+# who installed BOTH the standard editions and 32-bit editions 
+# side by side you will, of course, need to edit and match the shortcuts.
+#
+# Normal home end-users who have NOT installed both need not worry.
+#
+sudo rm -r -f /usr/share/applications/Firefox.desktop
+sudo rm -r -f /etc/skel/Desktop/Firefox.desktop
+rm -r -f /home/$USER/Desktop/Firefox.desktop
+#
+# Uncomment if you wish to also delete your configuration files too.
+# You should only do so if you have NO other installs of Firefox installed
+# Or if your previous install(s) were bad due to bad configuration.
+#
+# rm -r -f /home/$USER/.mozilla/
+# rm -r -f /home/$USER/.cache/mozilla/
+echo
+echo
+echo Thank you for using Mozilla Firefox.
+echo Firefox has been deleted and uninstalled. Per your request.
+echo Really sorry to see you go.
+echo Hope to see you again real soon.
+echo
+echo
+exit 0
