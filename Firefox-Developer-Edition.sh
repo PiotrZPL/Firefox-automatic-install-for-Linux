511ff69a9d20b73f168a69d03126c65dacbd4106
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-03-19 19:04:43 -0400

    added notes and streamlines install
    Added detail notes better explaining how the script works and streamlined the install process.

--- Firefox-Developer-Edition.sh
+++ Firefox-Developer-Edition.sh
@@ -1,16 +1,26 @@
 #!/bin/bash  
 echo
+# Wait for download notice
 echo "Please wait. I am downloading the latest version of Firefox Developer Edition"  
 echo
+# 4-second wait before beginning download
+# Gives user time to read the above sentence and understand what is happening
+sleep 4
+# Download
 wget -O FirefoxDeveloperEdition.tar.bz2 "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64"
 echo
+echo
+# Begin install notice
 echo "Installing Firefox Developer Edition"
+# Checks if distro has default opt path and if not found adds opt with default permissions
 sudo mkdir -p -m 755 /opt
+# Path where to be installed
 sudo mkdir /opt/firefox-developer-edition
+# Extracts to install path
 sudo tar xjf FirefoxDeveloperEdition.tar.bz2 -C /opt/firefox-developer-edition/
-echo "Applying update permissions"
+# Required permissions needed for Mozilla Firefox automatic update feature to work
 sudo chmod -R 757 /opt/firefox-developer-edition/firefox/
-echo "Creating icon"
+# Creating icon
 echo "[Desktop Entry]
 Name=Firefox Developer Edition
 GenericName=Web Browser
@@ -322,11 +332,10 @@ Name[zh_CN]=新建隐私浏览窗口
 Name[zh_TW]=新增隱私視窗
 Exec=/opt/firefox-developer-edition/firefox/firefox --private-window %u --class FirefoxDeveloperEdition" > Firefox-Developer-Edition.desktop
 echo -n
+# Makes icon executable allowing it to run Firefox (which is also executable)
 chmod +x Firefox-Developer-Edition.desktop
-echo "Installing icons"
 # Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...)
 sudo cp Firefox-Developer-Edition.desktop /usr/share/applications
-echo -n
 # Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all)
 for destdir in /home/*/Desktop/; do
     cp Firefox-Developer-Edition.desktop "$destdir" &&
@@ -336,9 +345,10 @@ echo -n
 # Adds a desktop icon to all FUTURE new login users (assuming you make any)
 sudo mkdir -p /etc/skel/Desktop
 sudo cp Firefox-Developer-Edition.desktop /etc/skel/Desktop
-echo "Cleaning up after myself"
+# Removes the temporary files no longer needed
 rm FirefoxDeveloperEdition.tar.bz2
 rm Firefox-Developer-Edition.desktop
+# Exit notice
 echo
 echo
 echo "Congratulations!"
