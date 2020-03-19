f5ba795dcb52a75521ea5f6bbdc6bb1654ef37fe
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-03-19 14:58:29 -0400

    improved notes
    Did a little housekeep by changing the layout and added a few basic notes

--- 32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh
@@ -3,32 +3,32 @@
 # This will uninstall and remove ALL editions of Firefox 32-bit. Use with care.
 # That which is done. Cannot be undone. Reinstalled, of course! But not undone.
 #
-echo
-sudo rm -r -f /opt/firefox-32bit/
-sudo rm -r -f /opt/firefox-beta-32bit/
-sudo rm -r -f /opt/firefox-developer-edition-32bit/
-sudo rm -r -f /opt/firefox-nightly-32bit/
-sudo rm -r -f /opt/firefox-esr-32bit/
-echo -n
-#
 # As noted in dev-doc.txt if you are a developer or enthusiast 
 # who installed BOTH the standard editions and 32-bit editions 
 # side by side you will, of course, need to edit and match the shortcuts.
 #
 # Normal home end-users who have NOT installed both need not worry.
 #
+echo
+# Installation
+sudo rm -r -f /opt/firefox-32bit/
+sudo rm -r -f /opt/firefox-beta-32bit/
+sudo rm -r -f /opt/firefox-developer-edition-32bit/
+sudo rm -r -f /opt/firefox-nightly-32bit/
+sudo rm -r -f /opt/firefox-esr-32bit/
+# Menu shotcuts
 sudo rm -r -f /usr/share/applications/Firefox.desktop
 sudo rm -r -f /usr/share/applications/Firefox-Beta.desktop
 sudo rm -r -f /usr/share/applications/Firefox-Developer-Edition.desktop
 sudo rm -r -f /usr/share/applications/Firefox-Nightly.desktop
 sudo rm -r -f /usr/share/applications/Firefox-ESR.desktop
-echo -n
+# Desktop shortcuts
 sudo rm -r -f /etc/skel/Desktop/Firefox.desktop
 sudo rm -r -f /etc/skel/Desktop/Firefox-Beta.desktop
 sudo rm -r -f /etc/skel/Desktop/Firefox-Developer-Edition.desktop
 sudo rm -r -f /etc/skel/Desktop/Firefox-Nightly.desktop
 sudo rm -r -f /etc/skel/Desktop/Firefox-ESR.desktop
-echo -n
+# Current deskop shortcuts
 rm -r -f /home/$USER/Desktop/Firefox.desktop
 rm -r -f /home/$USER/Desktop/Firefox-Beta.desktop
 rm -r -f /home/$USER/Desktop/Firefox-Developer-Edition.desktop
