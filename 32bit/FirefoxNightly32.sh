9f07f089cfb938d22764dc441e44c1e3acb231af
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-03-19 20:14:57 -0400

    added notes and streamlined install
    Added detail notes better explaining how the script works and streamlined the install process.

--- 32bit/FirefoxNightly32.sh
+++ 32bit/FirefoxNightly32.sh
@@ -1,16 +1,26 @@
 #!/bin/bash  
 echo
+# Wait for download notice
 echo "Please wait. I am downloading the latest version of Firefox Nightly"  
 echo
+# 4-second wait before beginning download. 
+# Gives user time to read the above sentence and understand what is happening
+sleep 4
+# Download
 wget -O FirefoxNightly32.tar.bz2 "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux"
 echo
+echo
+# Begin install notice
 echo "Installing Firefox Nightly"
+# Checks if distro has default opt path and if not found adds opt with default permissions
 sudo mkdir -p -m 755 /opt
+# Path where to be installed
 sudo mkdir /opt/firefox-nightly-32bit
+# Extracts to install path
 sudo tar xjf FirefoxNightly32.tar.bz2 -C /opt/firefox-nightly-32bit/
-echo "Applying update permissions"
+# Required permissions needed for Mozilla Firefox automatic update feature to work
 sudo chmod -R 757 /opt/firefox-nightly-32bit/firefox/
-echo "Creating icon"
+# Creating icon
 echo "[Desktop Entry]
 Name=Firefox Nightly
 GenericName=Web Browser
@@ -322,11 +332,10 @@ Name[zh_CN]=新建隐私浏览窗口
 Name[zh_TW]=新增隱私視窗
 Exec=/opt/firefox-nightly-32bit/firefox/firefox --private-window %u --class FirefoxNightly32" > Firefox-Nightly.desktop
 echo -n
+# Makes icon executable allowing it to run Firefox (which is also executable)
 chmod +x Firefox-Nightly.desktop
-echo "Installing icons"
 # Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...)
 sudo cp Firefox-Nightly.desktop /usr/share/applications
-echo -n
 # Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all)
 for destdir in /home/*/Desktop/; do
     cp Firefox-Nightly.desktop "$destdir" &&
@@ -336,9 +345,10 @@ echo -n
 # Adds a desktop icon to all FUTURE new login users (assuming you make any)
 sudo mkdir -p /etc/skel/Desktop
 sudo cp Firefox-Nightly.desktop /etc/skel/Desktop
-echo "Cleaning up after myself"
+# Removes the temporary files no longer needed
 rm FirefoxNightly32.tar.bz2
 rm Firefox-Nightly.desktop
+# Exit notice
 echo
 echo
 echo "Congratulations!"
