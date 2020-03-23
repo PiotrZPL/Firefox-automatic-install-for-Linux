85e0c77ad018f5d5b9c36e49bb40b81b36bafc59
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-03-23 14:34:03 -0400

    add notice
    a simple notice to remind the user which edition is being removed

--- 32bit/32bit-uninstallers/32beta-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32beta-UNINSTALL.sh
@@ -9,6 +9,10 @@
 # Normal home end-users who have NOT installed both need not worry.
 #
 echo
+# Uninstalling Firefox notice
+echo 'Uninstalling Firefox (beta edition)'
+echo
+echo
 # Installation
 sudo rm -r -f  /opt/firefox-beta-32bit/
 # Menu icons
