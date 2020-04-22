9c2d41f3f3f3e1771f35a8ddecb67ca026ddf792
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-04-22 15:04:44 -0400

    code clean 2.0.1
    cleaning code

--- 64bit/uninstallers/firefox-UNINSTALL.sh
+++ 64bit/uninstallers/firefox-UNINSTALL.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #
-# That which is done. Cannot be undone. Reinstalled, of course! But not undone.
-#
 echo
 # Uninstalling Firefox notice
 echo 'Uninstalling Firefox (stable edition)'
@@ -16,7 +14,7 @@ sudo rm -r -f /etc/skel/Desktop/Firefox.desktop
 # Current desktop shortcuts
 rm -r -f /home/$USER/Desktop/Firefox.desktop
 #
-# Uncomment if you wish to also delete your configuration files too.
+# Uncomment if you wish to also delete your configuration and profile files.
 # You should only do so if you have NO other installs of Firefox installed
 # Or if your previous install(s) were bad due to bad configuration.
 #
