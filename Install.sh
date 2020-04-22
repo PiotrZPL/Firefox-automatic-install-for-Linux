2c278ff19d6e7275ed0a6a6405de2c7f54a26c89
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-04-21 23:13:20 -0400

    initial release v2
    Speaking personally, I feel this release actually does exactly as the name of my project implies.  While originally, yes, the install was automated. Anyone could run for example FirefoxNightly.sh and Firefox Nightly would automatically install and this remains true. But with this release, I have taken it the next step and actually included a simple user interface. Making it so easy even someone who is presumably computer illiterate could read the documentation, copy and paste the single 2 commands provided, and from there just pick their choice by entering a simple number.  
    
    The only way I can actually imagine making this easier would be on Microsoft Windows and double-clicking setup.exe In regards to Linux, I don't yet think I can make it that easy... yet. lol

---
+++ Install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#
+# Firefox automatic install for Linux
+#
+#
+ PS3='Please enter your choice:'
+ options=("64-bit Firefox" "32-bit Firefox" "Quit")
+ while :
+ do
+ select opt in "${options[@]}"
+ do
+  case $opt in
+  "64-bit Firefox") clear; echo; echo; echo "You selected Firefox 64-bit"; echo; echo; chmod +x ./64bit/sub64.sh; ./64bit/sub64.sh; exit 0 ;;
+  "32-bit Firefox") clear; echo; echo; echo "You selected Firefox 32-bit"; echo; echo; chmod +x ./32bit/sub32.sh; ./32bit/sub32.sh; exit 0 ;;
+  "Quit") exit 0 ;;
+   *) echo "invalid option $REPLY" ; break ;;
+   esac
+  done
+  done
