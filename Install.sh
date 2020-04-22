47b987f25dba5b1932d20f2f9dad0cd346c794bd
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-04-22 15:44:18 -0400

    code clean 2.0.1
    A little house-cleaning

--- Install.sh
+++ Install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #
-#
 # Firefox automatic install for Linux
-#
 #
  PS3='Please enter your choice:'
  options=("64-bit Firefox" "32-bit Firefox" "Quit")
@@ -14,7 +12,7 @@
   "64-bit Firefox") clear; echo; echo; echo "You selected Firefox 64-bit"; echo; echo; chmod +x ./64bit/sub64.sh; ./64bit/sub64.sh; exit 0 ;;
   "32-bit Firefox") clear; echo; echo; echo "You selected Firefox 32-bit"; echo; echo; chmod +x ./32bit/sub32.sh; ./32bit/sub32.sh; exit 0 ;;
   "Quit") exit 0 ;;
-   *) echo "invalid option $REPLY" ; break ;;
+   *) echo "invalid option $REPLY"; break ;;
    esac
   done
   done
