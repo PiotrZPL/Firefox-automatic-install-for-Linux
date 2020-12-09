f5ee6f885091de9533388d3363fe83d5120097dc
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-12-08 22:22:26 -0500

    confirm before uninstall
    adds a confirmation before uninstalling

--- 64bit/uninstallers/firefox-dev-UNINSTALL.sh
+++ 64bit/uninstallers/firefox-dev-UNINSTALL.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 #
-echo
 # Uninstalling Firefox notice
-echo 'Uninstalling Firefox (developer edition)'
 echo
-echo
+ while true; do
+    read -p "Are you sure you wish to remove and delete 
+Mozilla Firefox Developer Edition release from your computer?" yn
+    case $yn in
+        [Yy]* ) clear; echo "Uninstalling Mozilla Firefox Developer Edition release"; echo; echo;
+# Small delay to give user time to read the above notice.
+sleep 3;
 # Installation
-sudo rm -r -f  /opt/firefox-developer-edition/
+sudo rm -r -f  /opt/firefox-developer-edition/ ;
 # Menu shotcuts
-sudo rm -r -f /usr/share/applications/Firefox-Developer-Edition.desktop
+sudo rm -r -f /usr/share/applications/Firefox-Developer-Edition.desktop ;
 # Desktop shotcuts
-sudo rm -r -f /etc/skel/Desktop/Firefox-Developer-Edition.desktop
+sudo rm -r -f /etc/skel/Desktop/Firefox-Developer-Edition.desktop ;
 # Current shortcuts
-rm -r -f /home/$USER/Desktop/Firefox-Developer-Edition.desktop
-#
+rm -r -f /home/$USER/Desktop/Firefox-Developer-Edition.desktop ;
 # Uncomment if you wish to also delete your configuration and profile files.
-# You should only do so if you have NO other installs of Firefox installed
-# Or if your previous install(s) were bad due to bad configuration.
-#
-# rm -r -f /home/$USER/.mozilla/
-# rm -r -f /home/$USER/.cache/mozilla/
-echo
-echo
-echo Thank you for using Mozilla Firefox.
-echo Firefox has been deleted and uninstalled. Per your request.
-echo Really sorry to see you go.
-echo Hope to see you again real soon.
-echo
-echo
+# rm -r -f /home/$USER/.mozilla/ ;
+# rm -r -f /home/$USER/.cache/mozilla/ ;
+echo; echo; echo "Thank you for using Mozilla Firefox.";
+echo "Firefox has been deleted and uninstalled. Per your request.";
+echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo; break ;;
+        [Nn]* ) echo; echo "You said, NO. Nothing has changed at this time."; echo; echo; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 exit 0
