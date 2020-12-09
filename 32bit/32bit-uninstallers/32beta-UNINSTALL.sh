5d40f6a0c5b2ac2bcdb1ea15257ffca761a403fb
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-12-08 21:10:00 -0500

    Are you sure?
    In previous releases, it was perhaps a little too easy to uninstall something. This new uninstaller simply asks, yes or no, to be sure.

--- 32bit/32bit-uninstallers/32beta-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32beta-UNINSTALL.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 #
-# As noted in dev-doc.txt if you are a developer or enthusiast 
-# who installed BOTH the standard editions and 32-bit editions 
-# You will, of course, need to edit and match the shortcuts with your intstall.
-#
-# Normal home end-users who have NOT installed both need not worry.
-echo
 # Uninstalling Firefox notice
-echo 'Uninstalling Firefox (beta edition)'
 echo
-echo
+ while true; do
+    read -p "Are you sure you wish to remove and delete 
+Mozilla Firefox Beta 32-bit release from your computer?" yn
+    case $yn in
+        [Yy]* ) clear; echo "Uninstalling Mozilla Firefox Beta 32-bit "; echo; echo;
+# Small delay to give user time to read the above notice.
+sleep 3;
 # Installation
-sudo rm -r -f  /opt/firefox-beta-32bit/
+sudo rm -r -f  /opt/firefox-beta-32bit/ ;
 # Menu icons
-sudo rm -r -f /usr/share/applications/Firefox-Beta.desktop
+sudo rm -r -f /usr/share/applications/Firefox-Beta-32bit.desktop ;
 # Desktop icons
-sudo rm -r -f /etc/skel/Desktop/Firefox-Beta.desktop
+sudo rm -r -f /etc/skel/Desktop/Firefox-Beta-32bit.desktop ;
 # Current desktop icons
-rm -r -f /home/$USER/Desktop/Firefox-Beta.desktop
-#
+rm -r -f /home/$USER/Desktop/Firefox-Beta-32bit.desktop ;
 # Uncomment if you wish to also delete your configuration and profile files.
-# You should only do so if you have NO other installs of Firefox installed
-# Or if your previous install(s) were bad due to bad configuration.
-#
-# rm -r -f /home/$USER/.mozilla/
-# rm -r -f /home/$USER/.cache/mozilla/
-echo
-echo
-echo Thank you for using Mozilla Firefox.
-echo Firefox has been deleted and uninstalled. Per your request.
-echo Really sorry to see you go.
-echo Hope to see you again real soon.
-echo
-echo
+# rm -r -f /home/$USER/.mozilla/ ;
+# rm -r -f /home/$USER/.cache/mozilla/ ;
+echo; echo; echo "Thank you for using Mozilla Firefox.";
+echo "Firefox has been deleted and uninstalled. Per your request.";
+echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo; break ;;
+        [Nn]* ) echo; echo "You said, NO. Nothing has changed at this time."; echo; echo; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 exit 0
