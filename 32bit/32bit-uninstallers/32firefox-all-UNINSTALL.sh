ac86e128ce5c93199c018a404f8ebef78d00b80a
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2020-12-08 21:14:37 -0500

    Update 32firefox-all-UNINSTALL.sh

--- 32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh
@@ -1,54 +1,56 @@
 #!/bin/bash
-# 
-# This will uninstall and remove ALL editions of Firefox 32-bit. Use with care.
 #
-# As noted in dev-doc.txt if you are a developer or enthusiast 
-# who installed BOTH the standard editions and 32-bit editions 
-# You will, of course, need to edit and match the shortcuts with your intstall.
-#
-# Normal home end-users who have NOT installed both need not worry.
-echo
 # Uninstalling Firefox notice
-echo Uninstalling ALL editons of Firefox 32 bit
 echo
-echo
+ while true; do
+    read -p "This will REMOVE ALL releases of Mozilla Firefox 32-bit on your computer.
+    
+Firefox
+Firefox Beta
+Firefox Developer Edition
+Firefox Nightly
+Firefox Extended Support Release
+
+That which is done, cannot be undone. Reinstalled, of course! But not undone.
+
+Are you sure wish to REMOVE ALL releases of 
+Mozilla Firefox 32-bit on your computer?" yn
+    case $yn in
+        [Yy]* ) clear; echo "Uninstalling ALL releases of Mozilla Firefox 32-bit"; echo; echo;
+# Small delay to give user time to read the above notice.
+sleep 3;
 # Installation
-sudo rm -r -f /opt/firefox-32bit/
-sudo rm -r -f /opt/firefox-beta-32bit/
-sudo rm -r -f /opt/firefox-developer-edition-32bit/
-sudo rm -r -f /opt/firefox-nightly-32bit/
-sudo rm -r -f /opt/firefox-esr-32bit/
+sudo rm -r -f /opt/firefox-32bit/ ;
+sudo rm -r -f /opt/firefox-beta-32bit/ ;
+sudo rm -r -f /opt/firefox-developer-edition-32bit/ ;
+sudo rm -r -f /opt/firefox-nightly-32bit/ ;
+sudo rm -r -f /opt/firefox-esr-32bit/ ;
 # Menu shotcuts
-sudo rm -r -f /usr/share/applications/Firefox.desktop
-sudo rm -r -f /usr/share/applications/Firefox-Beta.desktop
-sudo rm -r -f /usr/share/applications/Firefox-Developer-Edition.desktop
-sudo rm -r -f /usr/share/applications/Firefox-Nightly.desktop
-sudo rm -r -f /usr/share/applications/Firefox-ESR.desktop
+sudo rm -r -f /usr/share/applications/Firefox-32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox-Beta-32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox-Developer-Edition-32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox-Nightly-32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox-ESR-32bit.desktop ;
 # Desktop shortcuts
-sudo rm -r -f /etc/skel/Desktop/Firefox.desktop
-sudo rm -r -f /etc/skel/Desktop/Firefox-Beta.desktop
-sudo rm -r -f /etc/skel/Desktop/Firefox-Developer-Edition.desktop
-sudo rm -r -f /etc/skel/Desktop/Firefox-Nightly.desktop
-sudo rm -r -f /etc/skel/Desktop/Firefox-ESR.desktop
+sudo rm -r -f /etc/skel/Desktop/Firefox-32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox-Beta-32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox-Developer-Edition-32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox-Nightly-32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox-ESR-32bit.desktop ;
 # Current deskop shortcuts
-rm -r -f /home/$USER/Desktop/Firefox.desktop
-rm -r -f /home/$USER/Desktop/Firefox-Beta.desktop
-rm -r -f /home/$USER/Desktop/Firefox-Developer-Edition.desktop
-rm -r -f /home/$USER/Desktop/Firefox-Nightly.desktop
-rm -r -f /home/$USER/Desktop/Firefox-ESR.desktop
-#
+rm -r -f /home/$USER/Desktop/Firefox-32bit.desktop ;
+rm -r -f /home/$USER/Desktop/Firefox-Beta-32bit.desktop ;
+rm -r -f /home/$USER/Desktop/Firefox-Developer-Edition-32bit.desktop ;
+rm -r -f /home/$USER/Desktop/Firefox-Nightly-32bit.desktop ;
+rm -r -f /home/$USER/Desktop/Firefox-ESR-32bit.desktop ;
 # Uncomment if you wish to also delete your configuration and profile files.
-# You should only do so if you have NO other installs of Firefox installed
-# Or if your previous install(s) were bad due to bad configuration.
-#
-# rm -r -f /home/$USER/.mozilla/
-# rm -r -f /home/$USER/.cache/mozilla/
-echo
-echo
-echo Thank you for using Mozilla Firefox.
-echo Firefox has been deleted and uninstalled. Per your request.
-echo Really sorry to see you go.
-echo Hope to see you again real soon.
-echo
-echo
+# rm -r -f /home/$USER/.mozilla/ ;
+# rm -r -f /home/$USER/.cache/mozilla/ ;
+echo; echo; echo "Thank you for using Mozilla Firefox.";
+echo "Firefox has been deleted and uninstalled. Per your request.";
+echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo; break ;;
+        [Nn]* ) echo; echo "You said, NO. Nothing has changed at this time."; echo; echo; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 exit 0
