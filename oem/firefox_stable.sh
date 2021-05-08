1cf4e40431a2771a076add4e95c5a071ad4b0379
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2021-05-08 16:41:55 -0400

    oem firefox stable
    The silent firefox (current stable edition) installation file.

---
+++ oem/firefox_stable.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Firefox (current stable edition) - oem - silent install
+#
+# Download.
+wget -O FirefoxStable.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64";
+# Checks if distro has default opt path and if not found adds opt with default permissions.
+mkdir -p -m 755 /opt ;
+# Extracts to install path
+tar xjf FirefoxStable.tar.bz2 -C /opt/ ;
+# Required permissions needed for Mozilla Firefox automatic update feature to work.
+chmod -R 757 /opt/firefox/ ;
+# Start create icon script
+chmod +x ./fs64-icon.sh ; bash ./fs64-icon.sh ;
+# Give time for icon script to complete
+sleep 2;
+# Makes icon executable allowing it to run Firefox (which is also executable).
+chmod +x Firefox.desktop ;
+# Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...).
+cp Firefox.desktop /usr/share/applications ;
+# Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all).
+for destdir in /home/*/Desktop/; do
+    cp Firefox.desktop "$destdir" &&
+    chown --reference="$destdir" "$destdir/Firefox.desktop"
+done
+echo -n;
+# Adds a desktop icon to all FUTURE new login users (assuming you make any).
+mkdir -p /etc/skel/Desktop ; cp Firefox.desktop /etc/skel/Desktop ;
+# Removes the temporary files no longer needed.
+rm FirefoxStable.tar.bz2 ; rm Firefox.desktop ;
+# Exit
+exit 0
