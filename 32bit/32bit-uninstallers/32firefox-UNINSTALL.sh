79cb63e9013df27acbd9dd2d18052755ea058cc2
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2021-05-15 06:46:03 -0400

    Update 32firefox-UNINSTALL.sh

--- 32bit/32bit-uninstallers/32firefox-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32firefox-UNINSTALL.sh
@@ -17,9 +17,10 @@ sudo rm -r -f /usr/share/applications/Firefox-32bit.desktop ;
 sudo rm -r -f /etc/skel/Desktop/Firefox-32bit.desktop ;
 # Current desktop shortcuts
 sudo rm -r -f /home/*/Desktop/Firefox-32bit.desktop ;
-# Uncomment if you wish to also delete your configuration and profile files.
-# rm -r -f /home/$USER/.mozilla/ ;
-# rm -r -f /home/$USER/.cache/mozilla/ ;
+# Uncomment if you wish to delete file cache.
+# sudo rm -r -f /home/*/.cache/mozilla/firefox/*.default-release*/ ;
+# Uncomment if you wish to delete configuration and profile files.
+# sudo rm -r -f /home/*/.mozilla/firefox/*.default-release*/ ;
 echo; echo; echo "Thank you for using Mozilla Firefox.";
 echo "Firefox has been deleted and uninstalled. Per your request.";
 echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo; break ;;
