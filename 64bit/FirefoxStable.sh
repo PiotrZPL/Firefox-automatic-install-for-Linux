8c967b37129387a1f56b49908cddde5861aa04a5
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2021-05-15 03:49:46 -0400

    Mozilla Firefox naming
    Specified change from "Firefox" to "Mozilla Firefox."

--- 64bit/FirefoxStable.sh
+++ 64bit/FirefoxStable.sh
@@ -3,13 +3,13 @@
 # Installs Mozilla Firefox (stable release). To be used with Setup.sh
 #
 # Wait for download notice.
-echo; echo "Please wait. I am downloading the latest stable version of Firefox"; echo;
+echo; echo "Please wait. I am downloading the latest stable version of Mozilla Firefox"; echo;
 # 4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
 sleep 4;
 # Download.
 wget -O FirefoxStable.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64"; echo; echo;
 # Begin install notice.
-echo "Installing Firefox";
+echo "Installing Mozilla Firefox";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 sudo mkdir -p -m 755 /opt ;
 # Extracts to install path
@@ -21,22 +21,22 @@ chmod +x ./64bit/fs64-icon.sh ; bash ./64bit/fs64-icon.sh ;
 # Give time for icon script to complete
 sleep 2;
 # Makes icon executable allowing it to run Firefox (which is also executable).
-chmod +x Firefox.desktop ;
+chmod +x Mozilla-Firefox.desktop ;
 # Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...).
-sudo cp Firefox.desktop /usr/share/applications ;
+sudo cp Mozilla-Firefox.desktop /usr/share/applications ;
 # Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all).
 for destdir in /home/*/Desktop/; do
     cp Firefox.desktop "$destdir" &&
-    chown --reference="$destdir" "$destdir/Firefox.desktop"
+    chown --reference="$destdir" "$destdir/Mozilla-Firefox.desktop"
 done
 echo -n;
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
-sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox.desktop /etc/skel/Desktop ;
+sudo mkdir -p /etc/skel/Desktop ; sudo cp Mozilla-Firefox.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
-rm FirefoxStable.tar.bz2 ; rm Firefox.desktop ;
+rm FirefoxStable.tar.bz2 ; rm Mozilla-Firefox.desktop ;
 # Exit notice.
 echo; echo; echo "Congratulations!";
-echo "Firefox is now installed onto your computer.";
-echo "Firefox will update itself.";
+echo "Mozilla Firefox is now installed onto your computer.";
+echo "Mozilla Firefox will update itself.";
 echo "Happy browsing."; echo ; echo ;
 exit 0
