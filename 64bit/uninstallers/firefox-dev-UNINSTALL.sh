9f78445645f1b2f3f0fe160157530ffb93ab9bea
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2021-05-15 06:35:00 -0400

    Update firefox-dev-UNINSTALL.sh

--- 64bit/uninstallers/firefox-dev-UNINSTALL.sh
+++ 64bit/uninstallers/firefox-dev-UNINSTALL.sh
@@ -17,9 +17,10 @@ sudo rm -r -f /usr/share/applications/Firefox-Developer-Edition.desktop ;
 sudo rm -r -f /etc/skel/Desktop/Firefox-Developer-Edition.desktop ;
 # Current shortcuts
 sudo rm -r -f /home/*/Desktop/Firefox-Developer-Edition.desktop ;
-# Uncomment if you wish to also delete your configuration and profile files.
-# rm -r -f /home/$USER/.mozilla/ ;
-# rm -r -f /home/$USER/.cache/mozilla/ ;
+# Uncomment if you wish to delete file cache.
+# sudo rm -r -f /home/*/.cache/mozilla/firefox/*.dev-edition*/ ;
+# Uncomment if you wish to delete configuration and profile files.
+# sudo rm -r -f /home/*/.mozilla/firefox/*.dev-edition*/ ;
 echo; echo; echo "Thank you for using Mozilla Firefox.";
 echo "Firefox has been deleted and uninstalled. Per your request.";
 echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo; break ;;
