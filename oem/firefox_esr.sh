bd2fdc751135802a106c7d60741419bbbcab3651
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2021-11-24 13:26:47 -0500

    Update firefox_esr.sh

--- oem/firefox_esr.sh
+++ oem/firefox_esr.sh
@@ -2,6 +2,12 @@
 #
 # Firefox ESR (Extended Support Release) - oem - silent install
 #
+#
+# This installs BZIP2 on SUSE Linux distros and suppresses the error that ZYPPER is not found on non-SUSE distros.
+# In SUSE Linux, BZIP2 is required to extract the BZ2 archive downloaded from Mozilla.
+# While every major mainstream distribution includes a method to extract a BZ2 file, SUSE appears to be an exception.
+zypper refresh --force 2> /dev/null
+zypper install bzip2 2> /dev/null
 # Download
 wget -O FirefoxESR.tar.bz2 "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=linux64";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
