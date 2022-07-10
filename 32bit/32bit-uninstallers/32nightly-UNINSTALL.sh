bb97ced0f0cbc051e83cf3fa7ec50644cb17ed35
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-07-10 00:49:44 -0400

    Removed extra confimation
    Removing confirmation. The user instead is first given a warning before making their choice (from the sub menu). If they proceed, it was their choice to do so. They really do not need to be asked, "are you sure" a second time. Additionally, it cleans up and simplified the code.

--- 32bit/32bit-uninstallers/32nightly-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32nightly-UNINSTALL.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 #
 # Uninstalling Firefox notice
-echo
- while true; do
-    read -p "Are you sure you wish to remove and delete 
-Mozilla Firefox Nightly 32-bit release from your computer?" yn
-    case $yn in
-        [Yy]* ) clear; echo "Uninstalling Firefox Nightly 32-bit release"; echo; echo;
+echo "Uninstalling Firefox Nightly 32-bit release"; echo; echo;
 # Small delay to give user time to read the above notice.
 sleep 3;
 # Installation
@@ -23,9 +18,5 @@ sudo rm -r -f /home/*/Desktop/Firefox-Nightly-32bit.desktop ;
 # sudo rm -r -f /home/*/.mozilla/firefox/*.default-nightly*/ ;
 echo; echo; echo "Thank you for using Mozilla Firefox.";
 echo "Firefox has been deleted and uninstalled. Per your request.";
-echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo; break ;;
-        [Nn]* ) echo; echo "You said, NO. Nothing has changed at this time."; echo; echo; exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo;
 exit 0
