a5ecbcfa157791187dc850d09ff74f6f76999da3
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-07-22 15:55:07 -0400

    Universal Shell Edition
    A complete rewrite. This edition works using POSIX (sh), Bash shell, Dash shell, Z Shell (zsh) Korn Shell (ksh).
    
    When someone says something is impossible, make sure that is true.
    
    For example, Linux users and code developers are some of the biggest gatekeepers out there. Often when someone says something is impossible, what they really mean is they disprove of your efforts. So when someone says, "that's not possible" if it isn't going to cost you much of anything, and you have the time, make sure that is true.
    
    I wanted to make a shell script that could work in multiple shells (backward compatibly). I started in BASH and was informed, that's it, you're done, you cannot extend support beyond BASH. Wrong. My script now work in Bash, Dash, SH (posix), zsh (Z Shell), and ksh (Korn Shell). That's 5 different shells which everyone told me, you cannot work on backward compatibility. It's not possible and you shouldn't try.
    
    The benefit of being sure is you may also learn something along the way, that you may have otherwise not had you not tried.

--- Setup.sh
+++ Setup.sh
@@ -1,27 +1,41 @@
 #!/bin/sh
 #
 # Firefox automatic install for Linux
-#  Legacy Edition
-#   v2.8.5
+#  Universal Shell Edition
+#   v2.9.0
 #
-clear;
-echo;
- PS3='Please enter your choice:'
- menu=("Install 64-bit Firefox -- Recommended" "Install 32-bit Firefox -- legacy" "Uninstall 64-bit" "Uninstall 32-bit" "Quit")
- while :
- do
- select opt in "${menu[@]}"
- do
-  case $opt in
-  "Install 64-bit Firefox -- Recommended") clear; echo "You selected to install Firefox 64-bit"; echo; echo; chmod +x ./64bit/sub64.sh; ./64bit/sub64.sh; exit 0 ;;
-  "Install 32-bit Firefox -- legacy") clear; echo "You selected to install Firefox 32-bit"; echo; echo; chmod +x ./32bit/sub32.sh; ./32bit/sub32.sh; exit 0 ;;
-  "Uninstall 64-bit") clear; echo "You selected to uninstall 64-bit"; echo; echo; chmod +x ./64bit/uninstallers/unsub64.sh; ./64bit/uninstallers/unsub64.sh; exit 0 ;;
-  "Uninstall 32-bit") clear; echo "You selected to uninstall 32-bit"; echo; echo; chmod +x ./32bit/32bit-uninstallers/unsub32.sh; ./32bit/32bit-uninstallers/unsub32.sh; exit 0 ;;
-  "Quit") exit 0 ;;
-   *) echo "invalid option $REPLY"; break ;;
-   esac
-  done
-  done
-# Other than removing all my little developer notes. - Know a better way to improve this? Dare to speak up! One of the cool things about Linux and code, in general, is YOU (we all) make it possible, 
-# and we all start somewhere. An important thing to remember is no matter how good, or bad you are at coding, there will always be people who will discourage you. Only you can carry yourself forward. 
-# Take that first step and then another and another. Never give up. Never surrender. - As always, Happy Browsing!
+while true :
+do
+ clear;
+ echo;
+ echo "   M A I N - M E N U";
+ echo;
+ echo "1. Install 64-bit Firefox -- Recommended";
+ echo "2. Install 32-bit Firefox -- legacy";
+ echo "3. Uninstall 64-bit";
+ echo "4. Uninstall 32-bit"
+ echo "5. Exit";
+ echo;
+ echo "Please enter option [1 - 5]";
+ read -r opt
+ case $opt in
+  1) clear; echo; echo "You selected to install Firefox 64-bit"; echo; chmod +x ./64bit/sub64.sh; ./64bit/sub64.sh; exit 0 ;;
+
+  2) clear; echo; echo "You selected to install Firefox 32-bit"; echo; chmod +x ./32bit/sub32.sh; ./32bit/sub32.sh; exit 0 ;;
+
+  3) clear; echo; echo "You selected to uninstall 64-bit"; echo; chmod +x ./64bit/uninstallers/unsub64.sh; ./64bit/uninstallers/unsub64.sh; exit 0 ;;
+
+  4) clear; echo; echo "You selected to uninstall 32-bit"; echo; chmod +x ./32bit/32bit-uninstallers/unsub32.sh; ./32bit/32bit-uninstallers/unsub32.sh; exit 0 ;;
+
+  5) clear; echo; echo "Goodbye, $USER"; echo; exit 1;;
+
+  *) echo "$opt is an invaild option. Please select option between 1-5 only";
+     echo "Press the [enter] key to continue. . .";
+     read -r enterKey;
+esac
+done
+# Other than removing all my little developer notes. Do you know a better way to improve this? Dare to speak up!
+# One of the cool things about Linux and code, in general, is people like yourself. We all make it possible, and we all start somewhere.
+# An important thing to remember is no matter how good, or bad you are at coding, there will always be people who will discourage you.
+# Only you can carry yourself forward. Take that first step, and then another and another. Never give up. Never surrender.
+# -- As always, Happy Browsing!
