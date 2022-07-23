2f1018dd939b4929411d5414f4200cdad1d501df
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-07-22 20:53:01 -0400

    Universal Shell Edition
    A rewrite of the code to support POSIX shell (sh), Dash shell, Bash shell, Z Shell (zsh), and Korn Shell (ksh).

--- 64bit/sub-menu64.sh
+++ 64bit/sub-menu64.sh
@@ -1,22 +1,43 @@
 #!/bin/sh
 #
-# Firefox automatic install for Linux - This file to be used with Setup.sh
+# Firefox automatic install for Linux
+#  Universal Shell Edition
+#   v2.9.0
 #
- PS3='Please enter your choice:' 
- menu=("Mozilla Firefox" "Firefox Beta" "Firefox Developer Edition" "Firefox Nightly" "Firefox Extended Support Release" "Install ALL 64-bit editions" "Quit")
- while :
- do
- select opt in "${menu[@]}"
- do
-  case $opt in
-  "Mozilla Firefox") clear; echo "You selected Mozilla Firefox"; echo; echo; chmod +x ./64bit/FirefoxStable.sh; ./64bit/FirefoxStable.sh; break ;;
-  "Firefox Beta") clear; echo "You selected Firefox Beta"; echo; echo; chmod +x ./64bit/FirefoxBeta.sh; ./64bit/FirefoxBeta.sh; break ;;
-  "Firefox Developer Edition") clear; echo "You selected Firefox Developer Edition"; echo; echo; chmod +x ./64bit/Firefox-Developer-Edition.sh; ./64bit/Firefox-Developer-Edition.sh; break ;;
-  "Firefox Nightly") clear; echo "You selected Firefox Nightly"; echo; echo; chmod +x ./64bit/FirefoxNightly.sh; ./64bit/FirefoxNightly.sh; break ;;
-  "Firefox Extended Support Release") clear; echo "You selected Firefox Extended Support Release"; echo; echo; chmod +x ./64bit/FirefoxESR.sh; ./64bit/FirefoxESR.sh; break ;;
-  "Install ALL 64-bit editions") clear; echo "Notice:"; chmod +x ./64bit/firefox-all.sh; ./64bit/firefox-all.sh; break ;;
-  "Quit") exit 0 ;;
-   *) echo "invalid option $REPLY"; break ;;
-   esac
-  done
-  done
+# -- This file to be used with Setup.sh
+#
+while true :
+do
+ echo;
+ echo "   6 4 b i t - M E N U";
+ echo;
+ echo "1. Mozill Firefox";
+ echo "2. Firefox Beta";
+ echo "3. Firefox Developer Edition";
+ echo "4. Firefox Nightly";
+ echo "5. Firefox Extended Support Release";
+ echo "6. Install ALL 64-bit editions";
+ echo "7. Exit";
+ echo;
+ echo "Please enter option [1 - 7]";
+ read -r opt
+ case $opt in
+  1) clear; echo; echo "You selected Mozilla Firefox"; echo; chmod +x ./64bit/FirefoxStable.sh; ./64bit/FirefoxStable.sh; break ;;
+
+  2) clear; echo; echo "You selected Firefox Beta"; echo; chmod +x ./64bit/FirefoxBeta.sh; ./64bit/FirefoxBeta.sh; break ;;
+
+  3) clear; echo; echo "You selected Firefox Developer Edition"; echo; chmod +x ./64bit/Firefox-Developer-Edition.sh; ./64bit/Firefox-Developer-Edition.sh; break ;;
+
+  4) clear; echo; echo "You selected Firefox Nightly"; echo; chmod +x ./64bit/FirefoxNightly.sh; ./64bit/FirefoxNightly.sh; break ;;
+
+  5) clear; echo; echo "You selected Firefox Extended Support Release"; echo; chmod +x ./64bit/FirefoxESR.sh; ./64bit/FirefoxESR.sh; break ;;
+
+  6) clear; echo; echo "You selected to install ALL 64-bit editions"; echo; chmod +x ./64bit/firefox-all.sh; ./64bit/firefox-all.sh; break ;;
+
+  7) clear; echo; echo "Goodbye, $USER"; echo; exit 1;;
+
+  *) echo "$opt is an invaild option. Please select option between 1-7 only";
+     echo "Press the [enter] key to continue. . .";
+     read -r enterKey;
+esac
+done
