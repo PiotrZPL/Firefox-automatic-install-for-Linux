13c39b1df35e0412a812b82ec57ead3d1345b7c4
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-07-23 23:24:07 -0400

    Documentation
    This arguably does not change any code. It simply updates the documentation that better reflects the current release of v2.9.0.  A minor point release, 2.9.0.1 will be released to cover the minor footnotes I make.

--- 32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh
@@ -32,7 +32,9 @@ sudo rm -r -f /home/*/Desktop/Firefox-ESR-32bit.desktop ;
 sudo rm -r -f /home/*/.cache/mozilla/firefox/ ;
 # Configuration and profile files.
 sudo rm -r -f /home/*/.mozilla/firefox/ ;
+# exit notice
 echo; echo; echo "Thank you for using Mozilla Firefox.";
 echo "Firefox has been deleted and uninstalled. Per your request.";
 echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo;
+# exit
 exit 0
