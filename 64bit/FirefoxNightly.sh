81a2f42fe3705a7acf98cfb003e97c833c04c1fb
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-19 19:12:12 -0400

    rewrite
    This edition adds support for both CURL and WGET, as I became recently aware (and surprised) to learn some distributions do not include WGET. As someone who has always used openSUSE, Debian, Ubuntu, and alike this was news to me. The installer will not use whatever is available. However, if neither is available, it will correctly display a proper error message and safely exit without making any modifications. 
    
    Additionally, this rewrite replaces the echo command with prinf for further compatibility. As explained to me, not all user shells or distros are equal and a simple command such as echo can prove problematic. However, printf seems to be more universal (I am paraphrasing).

--- 64bit/FirefoxNightly.sh
+++ 64bit/FirefoxNightly.sh
@@ -2,14 +2,16 @@
 #
 # Installs Mozilla Firefox (nightly release). To be used with Setup.sh
 #
-# Wait for download notice.
-echo "Please wait. I am downloading the latest version of Firefox Nightly"; echo;
-# 4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
+# Make error.sh exactable so it can execut if needed.
+chmod +x ./64bit/error.sh;
+# Download notice.
+printf -- '\n\n%s\n\n\n\n' "Please wait. I am downloading the latest version of Mozilla Firefox Nightly.";
+#4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
 sleep 4;
-# Download.
-wget -O FirefoxNightly.tar.bz2 "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64"; echo; echo;
+# Download using wget with curl failback.
+wget -L -O "FirefoxNightly.tar.bz2" "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64" >/dev/null || curl -L -o "FirefoxNightly.tar.bz2" "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64" || /.error.sh ;
 # Begin install notice.
-echo "Installing Firefox Nightly";
+printf -- '\n\n\n%s\n\n' "Installing Mozilla Firefox Nightly.";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 sudo mkdir -p -m 755 /opt ;
 # Path where to be installed.
@@ -31,15 +33,15 @@ for destdir in /home/*/Desktop/; do
     cp Firefox-Nightly.desktop "$destdir" &&
     chown --reference="$destdir" "$destdir/Firefox-Nightly.desktop"
 done
-echo -n;
+printf "\n";
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
 sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox-Nightly.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
 rm FirefoxNightly.tar.bz2 ; rm Firefox-Nightly.desktop ;
 # Exit notice.
-echo; echo; echo "Congratulations!";
-echo "Firefox Nightly is now installed onto your computer.";
-echo "Firefox Nightly Edition will update itself.";
-echo "Happy browsing."; echo; echo;
+printf -- '%s\n' "" "" "" "Congratulations!" \
+  "Mozilla Firefox Nightly is now installed onto your computer." \
+  "Mozilla Firefox Nightly will update itself." \
+  "Happy browsing." "" ""
 # exit
 exit 0
