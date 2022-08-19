5b4b6025477a1e932d390548fc92f1cf5227ab1f
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-19 19:05:29 -0400

    rewrite
    This edition adds support for both CURL and WGET, as I became recently aware (and surprised) to learn some distributions do not include WGET. As someone who has always used openSUSE, Debian, Ubuntu, and alike this was news to me. The installer will not use whatever is available. However, if neither is available, it will correctly display a proper error message and safely exit without making any modifications. 
    
    Additionally, this rewrite replaces the echo command with prinf for further compatibility. As explained to me, not all user shells or distros are equal and a simple command such as echo can prove problematic. However, printf seems to be more universal (I am paraphrasing).

--- 64bit/FirefoxESR.sh
+++ 64bit/FirefoxESR.sh
@@ -2,14 +2,16 @@
 #
 # Installs Mozilla Firefox (extented support release). To be used with Setup.sh
 #
-# Wait for download notice.
-echo "Please wait. I am downloading the latest version of Firefox ESR"; echo;
-# 4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
+# Make error.sh exactable so it can execut if needed.
+chmod +x ./64bit/error.sh;
+# Download notice.
+printf -- '\n\n%s\n\n\n\n' "Please wait. I am downloading the latest version of Mozilla Firefox Extended Support Release.";
+#4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
 sleep 4;
-# Download
-wget -O FirefoxESR.tar.bz2 "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=linux64"; echo; echo;
+# Download using wget with curl failback.
+wget -L -O "FirefoxESR.tar.bz2" "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=linux64" >/dev/null || curl -L -o "FirefoxESR.tar.bz2" "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=linux64" || /.error.sh ;
 # Begin install notice.
-echo "Installing Firefox Extended Support Release"
+printf -- '\n\n\n%s\n\n' "Installing Mozilla Firefox Extended Support Release.";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 sudo mkdir -p -m 755 /opt ;
 # Path where to be installed.
@@ -31,15 +33,15 @@ for destdir in /home/*/Desktop/; do
     cp Firefox-ESR.desktop "$destdir" &&
     chown --reference="$destdir" "$destdir/Firefox-ESR.desktop"
 done
-echo -n;
+printf "\n";
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
 sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox-ESR.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
 rm FirefoxESR.tar.bz2 ; rm Firefox-ESR.desktop ;
 # Exit notice.
-echo; echo; echo "Congratulations!";
-echo "Firefox ESR is now installed onto your computer.";
-echo "Firefox Extended Support Release edition will update itself.";
-echo "Happy browsing."; echo; echo;
+printf -- '%s\n' "" "" "" "Congratulations!" \
+  "Mozilla Firefox Extended Support Release is now installed onto your computer." \
+  "Mozilla Firefox Extended Support Release will update itself." \
+  "Happy browsing." "" ""
 # exit
 exit 0
