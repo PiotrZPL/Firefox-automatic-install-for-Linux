ae77c76342866eb9f854bcddf7c097ed730042da
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 00:47:39 -0400

    replace echo with printf

--- 64bit/icon-developer64.sh
+++ 64bit/icon-developer64.sh
@@ -3,7 +3,7 @@
 # This script uses echo to generate a icon shortcut file. - Firefox Developer Edition
 #
 # Creating icon
-echo "[Desktop Entry]
+printf "[Desktop Entry]
 Name=Firefox Developer Edition
 GenericName=Web Browser
 GenericName[ar]=متصفح وِب
