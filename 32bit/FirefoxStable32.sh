4d53d93506a2a8bfe33581822357fbffffd900f9
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 05:24:03 -0400

    Curl and Wget
    If you're an openSUSE or Debian user or similar, you may be surprised to learn WGET is not standard in most distributions.  When making this script, I had tested more than my fair share off of distrowatch.com and WGET had been ironically included in all of them.  I was surprised to learn that this is not the norm.  Going forward, the setup will not pick between WGET and CURL, ensuring compatibility. And should that fail, you will be greeted with a friendly error message.

--- 32bit/FirefoxStable32.sh
+++ 32bit/FirefoxStable32.sh
@@ -5,13 +5,13 @@
 # Make error.sh exactable so it can execut if needed.
 chmod +x ./32bit/error.sh;
 # Download notice.
-printf -- '\n\n%s\n\n\n\n' "Please wait. I am download the latest version of Mozilla Firefox.";
+printf -- '\n\n%s\n\n\n\n' " Please wait. I am download the latest version of Mozilla Firefox.";
 #4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
 sleep 4;
 # Download using wget with curl failback.
 wget -L -O "FirefoxStable.tar.bz2" "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux" >/dev/null || curl -L -o "FirefoxStable.tar.bz2" "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux" || /.error.sh ;
 # Begin install notice.
-printf -- '\n\n\n%s\n\n' "Installing Mozilla Firefox";
+printf -- '\n\n\n%s\n\n' " Installing Mozilla Firefox";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 sudo mkdir -p -m 755 /opt ;
 # Extracts to install path
@@ -37,9 +37,9 @@ sudo mkdir -p /etc/skel/Desktop ; sudo cp Mozilla-Firefox.desktop /etc/skel/Desk
 # Removes the temporary files no longer needed.
 rm FirefoxStable.tar.bz2 ; rm Mozilla-Firefox.desktop ;
 # Exit notice.
-printf -- '%s\n' "" "" "" "Congratulations!" \
-  "Mozilla Firefox is now installed onto your computer." \
-  "Mozilla Firefox will update itself." \
-  "Happy browsing." "" ""
+printf -- '%s\n' "" "" "" " Congratulations!" \
+  " Mozilla Firefox is now installed onto your computer." \
+  " Mozilla Firefox will update itself." \
+  " Happy browsing." "" ""
 # exit
 exit 0
