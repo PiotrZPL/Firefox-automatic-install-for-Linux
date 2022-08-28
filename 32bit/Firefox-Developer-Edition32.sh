1f703463d79f193c73b595493d1fde0a27c67274
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 02:14:50 -0400

    CURL support with WGET
    My distro of choice is usually openSUSE or Debian or alike, and WGET was provided by default. I recall testing the top 5-10 on distrowatch.com, and WGET was always standard. Imagine my surprise when I learned that may technically be the exception and not the norm. -- This release will not use whichever is available (WGET or CURL). And if either fails, display a proper error message. 
    
    Additionally, I have replaced "echo" with "printf" as I was made aware that echo is not universal in how it functions or performs in every distro or shell. Whenever possible, it has been my goal to maximize compatibility, and to that end, I am retiring echo in replacing it with printf.

--- 32bit/Firefox-Developer-Edition32.sh
+++ 32bit/Firefox-Developer-Edition32.sh
@@ -2,14 +2,16 @@
 #
 # Installs Mozilla Firefox (developer edition). To be used with Setup.sh
 #
-# Wait for download notice.
-echo "Please wait. I am downloading the latest version of Firefox Developer Edition"; echo;
-# 4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
+# Make error.sh exactable so it can execut if needed.
+chmod +x ./32bit/error.sh;
+# Download notice.
+printf -- '\n\n%s\n\n\n\n' " Please wait. I am downloading the latest version of Mozilla Firefox Developer Edition.";
+#4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
 sleep 4;
-# Download.
-wget -O FirefoxDeveloperEdition32.tar.bz2 "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux"; echo; echo;
+# Download using wget with curl failback.
+wget -L -O "FirefoxDeveloperEdition.tar.bz2" "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux" >/dev/null || curl -L -o "FirefoxDeveloperEdition.tar.bz2" "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux" || /.error.sh ;
 # Begin install notice.
-echo "Installing Firefox Developer Edition";
+printf -- '\n\n\n%s\n\n' " Installing Mozilla Firefox Developer Edition.";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 sudo mkdir -p -m 755 /opt ;
 # Path where to be installed.
@@ -31,15 +33,15 @@ for destdir in /home/*/Desktop/; do
     cp Firefox-Developer-Edition-32bit.desktop "$destdir" &&
     chown --reference="$destdir" "$destdir/Firefox-Developer-Edition-32bit.desktop"
 done
-echo -n;
+printf "\n";
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
 sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox-Developer-Edition-32bit.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
 rm FirefoxDeveloperEdition32.tar.bz2 ; rm Firefox-Developer-Edition-32bit.desktop ;
 # Exit notice.
-echo; echo; echo "Congratulations!";
-echo "Firefox Developer Edition is now installed onto your computer.";
-echo "Firefox Developer Edition will update itself.";
-echo "Happy browsing."; echo; echo;
+printf -- '%s\n' "" "" "" " Congratulations!" \
+  " Mozilla Firefox Developer Edition is now installed onto your computer." \
+  " Mozilla Firefox Developer Edition will update itself." \
+  " Happy browsing." "" ""
 # exit
 exit 0
