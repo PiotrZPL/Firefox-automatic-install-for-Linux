ff025dd99ef8c4fd0fa29ef84e2d1c5d0107af7b
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 02:30:23 -0400

    Curl support with Wget
    I was recently surprised to learn that WGET, is not commonly included in most distros. Being that my distro of choice would normally be OpenSUSE, Debian, Ubuntu, and alike. Stranger still, I recall testing the top 5-10 distros listed on distrowatch.com to verify the functionality of my script, and in all instances, WGET was included.  In any case, it will now use whichever is available (CURL or WGET), and if something fails, it will display a proper error message.

--- 32bit/FirefoxBeta32.sh
+++ 32bit/FirefoxBeta32.sh
@@ -2,14 +2,16 @@
 #
 # Installs Mozilla Firefox (beta release). To be used with Setup.sh
 #
-# Wait for download notice.
-echo "Please wait. I am downloading the latest version of Firefox Beta"; echo;
-# 4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
+# Make error.sh exactable so it can execut if needed.
+chmod +x ./32bit/error.sh;
+# Download notice.
+printf -- '\n\n%s\n\n\n\n' " Please wait. I am downloading the latest version of Mozilla Firefox Beta.";
+#4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
 sleep 4;
-# Download.
-wget -O FirefoxBeta32.tar.bz2 "https://download.mozilla.org/?product=firefox-beta-latest-ssl&os=linux"; echo; echo;
+# Download using wget with curl failback.
+wget -L -O "FirefoxBeta.tar.bz2" "https://download.mozilla.org/?product=firefox-beta-latest-ssl&os=linux" >/dev/null || curl -L -o "FirefoxBeta.tar.bz2" "https://download.mozilla.org/?product=firefox-beta-latest-ssl&os=linux" || /.error.sh ;
 # Begin install notice.
-echo "Installing Firefox Beta"
+printf -- '\n\n\n%s\n\n' " Installing Mozilla Firefox Beta.";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 sudo mkdir -p -m 755 /opt ;
 # Path where to be installed.
@@ -31,15 +33,15 @@ for destdir in /home/*/Desktop/; do
     cp Firefox-Beta-32bit.desktop "$destdir" &&
     chown --reference="$destdir" "$destdir/Firefox-Beta-32bit.desktop"
 done
-echo -n;
+printf "\n";
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
 sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox-Beta-32bit.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
 rm FirefoxBeta32.tar.bz2 ; rm Firefox-Beta-32bit.desktop ;
 # Exit notice.
-echo; echo; echo "Congratulations!";
-echo "Firefox Beta is now installed onto your computer.";
-echo "Firefox Beta will update itself.";
-echo "Happy browsing."; echo; echo;
+printf -- '%s\n' "" "" "" " Congratulations!" \
+  " Mozilla Firefox Beta is now installed onto your computer." \
+  " Mozilla Firefox Beta will update itself." \
+  " Happy browsing." "" ""
 # exit
 exit 0
