e0522e8cd3da2d932240f23acfcc2a66d948350b
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 01:34:27 -0400

    visual spacing
    Depending on the distro, sometimes the text in the window or terminal is right up to the very edge of the screen (or window), some times it was seamless.  At the very least, the bare minimum spacing away from the edge of the terminal window or screen.

--- 32bit/32bit-uninstallers/32nightly-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32nightly-UNINSTALL.sh
@@ -21,8 +21,8 @@ sudo rm -r -f /home/*/Desktop/Firefox-Nightly-32bit.desktop ;
 # sudo rm -r -f /home/*/.mozilla/firefox/*.default-nightly*/ ;
 #
 # exit notice
-printf -- '%s\n' "" "" "" "Thank you for using Mozilla Firefox." \
-"Firefox has been deleted and uninstalled. Per your request." \
-"Really sorry to see you go. Hope to see you again real soon." "" ""
+printf -- '%s\n' "" "" "" " Thank you for using Mozilla Firefox." \
+" Firefox has been deleted and uninstalled. Per your request." \
+" Really sorry to see you go. Hope to see you again real soon." "" ""
 # exit
 exit 0
