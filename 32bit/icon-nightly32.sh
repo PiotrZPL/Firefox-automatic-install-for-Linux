676d0f1c26f5f4c89624b45b4ef3780ef06b4ff1
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 05:40:52 -0400

    echo now printf
    It has come to my attention that "echo" is not as nearly universal as I once thought, depending on your distro and shell. Going forward, the development will use "printf"

--- 32bit/icon-nightly32.sh
+++ 32bit/icon-nightly32.sh
@@ -2,7 +2,7 @@
 #
 # Firefox Nightly release icon - 32bit
 # Creating icon.
-echo "[Desktop Entry]
+printf "[Desktop Entry]
 # The file name should be different from the default 64-bit names (keeps compatibility so you can install both if you choose).
 Name=Firefox Nightly 32bit
 #
