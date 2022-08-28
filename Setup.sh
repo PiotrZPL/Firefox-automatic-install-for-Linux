55fe3f61ad7496305005a0484f54f0ee9b3ec294
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 06:39:13 -0400

    2.9.0.3
    I extended download support because, as it turns out, I was living in a bubble. All my distributions of choice include WGET by default. So I incorrectly assumed that was the norm. Ironically, I had even tested many distros off of distrowatch.com, and they reassured my belief that WGET was the norm. It's not as CURL is widely more common. The development now will automatically search to use either WGET or CURL. If neither is available, you will be greeted by a friendly error message.
    
    Another new feature I added to the uninstall menu is "purge."  This is ideal for developers or power users who need a fresh new start.
    
    Lastly, it came to my attention and understanding that "echo" is not as nearly widely universal depending on what shell or distribution you are running. "Printf," however, is, so the whole development, including the personal installer and OEM, has retired "echo" in favor of "printf."  For the user interface,  a whole new rewrite.

--- Setup.sh
+++ Setup.sh
@@ -1,37 +1,38 @@
 #!/bin/sh
 #
 # Firefox automatic install for Linux
-#  Universal Shell Edition
-#   v2.9.0
+#   v2.9.0.3
 #
 while true :
 do
  clear;
- echo;
- echo "   M A I N - M E N U";
- echo;
- echo "1. Install 64-bit Firefox -- Recommended";
- echo "2. Install 32-bit Firefox -- legacy";
- echo "3. Uninstall 64-bit";
- echo "4. Uninstall 32-bit";
- echo "5. Exit";
- echo;
- echo "Please enter option [1 - 5]";
+ printf -- '\n\n%s\n\n\n\n' " ";
+ printf -- '%s\n' "   M A I N - M E N U" \
+ " " \
+ " 1. Install 64-bit Firefox -- Recommended" \
+ " 2. Install 32-bit Firefox -- legacy" \
+ " 3. Uninstall 64-bit" \
+ " 4. Uninstall 32-bit" \
+ " 5. Exit" \
+ "" ""
+ printf " Please enter option [1 - 5]";
  read -r opt
  case $opt in
-  1) clear; echo; echo "You selected to install Firefox 64-bit"; echo; chmod +x ./64bit/sub-menu64.sh; ./64bit/sub-menu64.sh; exit 0 ;;
+  1) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected to install Firefox 64-bit"; chmod +x ./64bit/sub-menu64.sh; ./64bit/sub-menu64.sh; exit 0 ;;
 
-  2) clear; echo; echo "You selected to install Firefox 32-bit"; echo; chmod +x ./32bit/sub-menu32.sh; ./32bit/sub-menu32.sh; exit 0 ;;
+  2) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected to install Firefox 32-bit"; chmod +x ./32bit/sub-menu32.sh; ./32bit/sub-menu32.sh; exit 0 ;;
 
-  3) clear; echo; echo "You selected to uninstall 64-bit"; echo; chmod +x ./64bit/uninstallers/uninstall-menu64.sh; ./64bit/uninstallers/uninstall-menu64.sh; exit 0 ;;
+  3) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected to uninstall 64-bit"; chmod +x ./64bit/uninstallers/uninstall-menu64.sh; ./64bit/uninstallers/uninstall-menu64.sh; exit 0 ;;
 
-  4) clear; echo; echo "You selected to uninstall 32-bit"; echo; chmod +x ./32bit/32bit-uninstallers/uninstall-menu32.sh; ./32bit/32bit-uninstallers/uninstall-menu32.sh; exit 0 ;;
+  4) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected to uninstall 32-bit"; chmod +x ./32bit/32bit-uninstallers/uninstall-menu32.sh; ./32bit/32bit-uninstallers/uninstall-menu32.sh; exit 0 ;;
 
-  5) clear; echo; echo "Goodbye, $USER"; echo; exit 1;;
+  5) clear; printf -- '\n\n\n%s\n\n\n\n' " Goodbye, $USER"; exit 1;;
 
-  *) echo "$opt is an invaild option. Please select option between 1-5 only";
-     echo "Press the [enter] key to continue. . .";
+  *) clear;
+     printf -- '\n\n%s\n' " $opt is an invaild option. Please select option between 1-7 only" \
+     " Press the [enter] key to continue. . ."
      read -r enterKey;
+     clear;
 esac
 done
 # Other than removing all my little developer notes. Do you know a better way to improve this? Dare to speak up!
