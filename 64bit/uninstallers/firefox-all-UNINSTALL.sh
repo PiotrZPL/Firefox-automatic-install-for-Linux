6e66dfd40baf80ba6285e6409014216cf946c0c5
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 19:14:11 -0400

    merge
    2.9.0.2 vs 2.9.0.3 could not merge since I applied a temporary patch fix. This adds the development code to the main branch.

--- 64bit/uninstallers/firefox-all-UNINSTALL.sh
+++ 64bit/uninstallers/firefox-all-UNINSTALL.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
+# Firefox Automatic Install for Linux -- Uninstall ALL 64-bit editions -- File can be used independently.
+#
 # Uninstalling Firefox notice
-echo "Uninstalling ALL editons of Mozilla Firefox (64 bit)"; echo; echo;
+printf -- '\n\n%s\n\n\n\n' " Uninstalling ALL releases of Mozilla Firefox 64-bit";
 # Small delay to give user time to read the above notice.
 sleep 3;
 # Installation
@@ -28,13 +30,13 @@ sudo rm -r -f /home/*/Desktop/Firefox-Beta.desktop ;
 sudo rm -r -f /home/*/Desktop/Firefox-Developer-Edition.desktop ;
 sudo rm -r -f /home/*/Desktop/Firefox-Nightly.desktop ;
 sudo rm -r -f /home/*/Desktop/Firefox-ESR.desktop ;
-# Uncomment if you wish to delete file cache.
+# Cache files
 # sudo rm -r -f /home/*/.cache/mozilla/firefox/ ;
 # Configuration and profile files.
 # sudo rm -r -f /home/*/.mozilla/firefox/ ;
 # Exit notice
-echo; echo; echo "Thank you for using Mozilla Firefox.";
-echo "Firefox has been deleted and uninstalled. Per your request.";
-echo "Really sorry to see you go. Hope to see you again real soon."; echo; echo;
+printf -- '%s\n' "" "" "" " Thank you for using Mozilla Firefox." \
+" Firefox has been deleted and uninstalled. Per your request." \
+" Really sorry to see you go. Hope to see you again real soon." "" ""
 # Exit
 exit 0
