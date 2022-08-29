e89dbd0add9969fe974552aa2ce0bfd6eb6da73a
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 23:07:24 -0400

    excess line spacing
    The user notices experienced a tag bit of excessive line spacing. This corrects that issue.

--- 32bit/sub-menu32.sh
+++ 32bit/sub-menu32.sh
@@ -7,7 +7,7 @@
 #
 while true :
 do
- printf -- '\n\n%s\n\n\n\n' " ";
+ printf -- '\n%s\n' " ";
  printf -- '%s\n' "   6 4 b i t - M E N U" \
  " " \
  " 1. Mozill Firefox" \
@@ -21,19 +21,19 @@ do
  printf " Please enter option [1 - 7]";
  read -r opt
  case $opt in
-  1) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Mozilla Firefox"; chmod +x ./32bit/FirefoxStable32.sh; ./32bit/FirefoxStable32.sh; break ;;
+  1) clear; printf -- '\n%s\n\n' " You selected Mozilla Firefox"; chmod +x ./32bit/FirefoxStable32.sh; ./32bit/FirefoxStable32.sh; break ;;
 
-  2) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Beta"; chmod +x ./32bit/FirefoxBeta32.sh; ./32bit/FirefoxBeta32.sh; break ;;
+  2) clear; printf -- '\n%s\n\n' " You selected Firefox Beta"; chmod +x ./32bit/FirefoxBeta32.sh; ./32bit/FirefoxBeta32.sh; break ;;
 
-  3) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Developer Edition"; chmod +x ./32bit/Firefox-Developer-Edition32.sh; ./32bit/Firefox-Developer-Edition32.sh; break ;;
+  3) clear; printf -- '\n%s\n\n' " You selected Firefox Developer Edition"; chmod +x ./32bit/Firefox-Developer-Edition32.sh; ./32bit/Firefox-Developer-Edition32.sh; break ;;
 
-  4) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Nightly"; chmod +x ./32bit/FirefoxNightly32.sh; ./32bit/FirefoxNightly32.sh; break ;;
+  4) clear; printf -- '\n%s\n\n' " You selected Firefox Nightly"; chmod +x ./32bit/FirefoxNightly32.sh; ./32bit/FirefoxNightly32.sh; break ;;
 
-  5) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Extended Support Release"; chmod +x ./32bit/FirefoxESR32.sh; ./32bit/FirefoxESR32.sh; break ;;
+  5) clear; printf -- '\n%s\n\n' " You selected Firefox Extended Support Release"; chmod +x ./32bit/FirefoxESR32.sh; ./32bit/FirefoxESR32.sh; break ;;
 
-  6) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected to install ALL 32-bit editions"; chmod +x ./32bit/firefox-all32.sh; ./32bit/firefox-all32.sh; break ;;
+  6) clear; printf -- '\n%s\n\n' " You selected to install ALL 32-bit editions"; chmod +x ./32bit/firefox-all32.sh; ./32bit/firefox-all32.sh; break ;;
 
-  7) clear; printf -- '\n\n\n%s\n\n\n\n' " Goodbye, $USER"; exit 1;;
+  7) clear; printf -- '\n%s\n\n' " Goodbye, $USER"; exit 1;;
 
   *) clear;
      printf -- '\n\n%s\n' " $opt is an invaild option. Please select option between 1-7 only" \
