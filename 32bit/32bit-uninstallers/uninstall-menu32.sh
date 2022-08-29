6f3c29f1238b4e3d657a91c9f8b9135aae5053dd
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 23:10:14 -0400

    excess line space fix
    The user notifications on the menu experienced a bit of an excessive line space issue. This resolve that issue.

--- 32bit/32bit-uninstallers/uninstall-menu32.sh
+++ 32bit/32bit-uninstallers/uninstall-menu32.sh
@@ -8,7 +8,7 @@
 # That which is done, cannot be undone. Reinstalled, of course! But not undone.
 while true :
 do
-printf -- '\n\n%s\n\n\n\n' " ";
+printf -- '\n%s\n' " ";
 printf -- '%s\n' "   U N I N S T A L L - 3 2 b i t - M E N U" \
 " " \
 " CAUTION - You are about to remove and delete" \
@@ -28,21 +28,21 @@ printf -- '%s\n' "   U N I N S T A L L - 3 2 b i t - M E N U" \
  printf " Please enter option [1 - 7]";
  read -r opt
  case $opt in
-  1) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Mozilla Firefox"; chmod +x ./32bit/32bit-uninstallers/32firefox-UNINSTALL.sh; ./32bit/32bit-uninstallers/32firefox-UNINSTALL.sh; break ;;
+  1) clear; printf -- '\n%s\n\n' " You selected Mozilla Firefox"; chmod +x ./32bit/32bit-uninstallers/32firefox-UNINSTALL.sh; ./32bit/32bit-uninstallers/32firefox-UNINSTALL.sh; break ;;
 
-  2) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Beta"; chmod +x ./32bit/32bit-uninstallers/32beta-UNINSTALL.sh; ./32bit/32bit-uninstallers/32beta-UNINSTALL.sh; break ;;
+  2) clear; printf -- '\n%s\n\n' " You selected Firefox Beta"; chmod +x ./32bit/32bit-uninstallers/32beta-UNINSTALL.sh; ./32bit/32bit-uninstallers/32beta-UNINSTALL.sh; break ;;
 
-  3) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Developer Edition"; chmod +x ./32bit/32bit-uninstallers/32firefox-dev-UNINSTALL.sh; ./32bit/32bit-uninstallers/32firefox-dev-UNINSTALL.sh; break ;;
+  3) clear; printf -- '\n%s\n\n' " You selected Firefox Developer Edition"; chmod +x ./32bit/32bit-uninstallers/32firefox-dev-UNINSTALL.sh; ./32bit/32bit-uninstallers/32firefox-dev-UNINSTALL.sh; break ;;
 
-  4) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Nightly"; chmod +x ./32bit/32bit-uninstallers/32nightly-UNINSTALL.sh; ./32bit/32bit-uninstallers/32nightly-UNINSTALL.sh; break ;;
+  4) clear; printf -- '\n%s\n\n' " You selected Firefox Nightly"; chmod +x ./32bit/32bit-uninstallers/32nightly-UNINSTALL.sh; ./32bit/32bit-uninstallers/32nightly-UNINSTALL.sh; break ;;
 
-  5) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Extended Support Release"; chmod +x ./32bit/32bit-uninstallers/32esr-UNINSTALL.sh; ./32bit/32bit-uninstallers/32esr-UNINSTALL.sh; break ;;
+  5) clear; printf -- '\n%s\n\n' " You selected Firefox Extended Support Release"; chmod +x ./32bit/32bit-uninstallers/32esr-UNINSTALL.sh; ./32bit/32bit-uninstallers/32esr-UNINSTALL.sh; break ;;
 
-  6) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected to remove ALL 32-bit editions"; chmod +x ./32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh; ./32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh; break ;;
+  6) clear; printf -- '\n%s\n\n' " You selected to remove ALL 32-bit editions"; chmod +x ./32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh; ./32bit/32bit-uninstallers/32firefox-all-UNINSTALL.sh; break ;;
 
-  7) clear; printf -- '\n\n\n%s\n\n\n\n' " Goodbye, $USER"; exit 1;;
+  7) clear; printf -- '\n%s\n\n' " Goodbye, $USER"; exit 1;;
   
-  66) clear; printf -- '\n\n\n%s\n\n\n\n' " Per your request, $USER. Execute Order 66."; chmod +x ./32bit/32bit-uninstallers/PURGE-32.sh; ./32bit/32bit-uninstallers/PURGE-32.sh; break ;;
+  66) clear; printf -- '\n%s\n\n' " Per your request, $USER. Execute Order 66."; chmod +x ./32bit/32bit-uninstallers/PURGE-32.sh; ./32bit/32bit-uninstallers/PURGE-32.sh; break ;;
 
   *) clear;
      printf -- '\n\n%s\n' " $opt is an invaild option. Please select option between 1-7 only" \
