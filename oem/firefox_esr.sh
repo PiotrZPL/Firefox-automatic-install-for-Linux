a212c88ec2f5c4bc4273c8a6cb618417ace8bcfb
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 20:32:30 -0400

    CURL support added
    I was surprised to learn that "WGET" is not usually included in most distributions. My distribution of choice is often openSUSE or Debian and alike. I even recall testing more than a dozen random distros from distowatch.com, but they included "WGET." In short, I was living in a bubble. -- The script now will search for "WGET" and "CURL."  A friendly error message will display if neither is available.

--- oem/firefox_esr.sh
+++ oem/firefox_esr.sh
@@ -2,8 +2,16 @@
 #
 # Firefox ESR (Extended Support Release) - oem - silent install
 #
-# Download
-wget -O FirefoxESR.tar.bz2 "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=linux64";
+# Make error.sh exactable so it can execut if needed.
+chmod +x ./error.sh;
+# Download notice.
+# printf -- '\n\n%s\n\n\n\n' " Please wait. I am downloading the latest version of Mozilla Firefox Extended Support Release.";
+#4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
+# sleep 4;
+# Download using wget with curl failback.
+wget -L -O "FirefoxESR.tar.bz2" "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=linux64" >/dev/null || curl -L -o "FirefoxESR.tar.bz2" "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=linux64" || /.error.sh ;
+# Begin install notice.
+# printf -- '\n\n\n%s\n\n' " Installing Mozilla Firefox Extended Support Release.";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 mkdir -p -m 755 /opt ;
 # Path where to be installed.
@@ -13,7 +21,7 @@ tar xjf FirefoxESR.tar.bz2 -C /opt/firefox-esr/ ;
 # Required permissions needed for Mozilla Firefox automatic update feature to work.
 chmod -R 757 /opt/firefox-esr/firefox/ ;
 # Start create icon script.
-chmod +x ./extended64-icon.sh ; bash ./extended64-icon.sh ;
+chmod +x ./icon-extended64.sh ; bash ./icon-extended64.sh ;
 # Give time for icon script to complete.
 sleep 2;
 # Makes icon executable allowing it to run Firefox (which is also executable).
@@ -25,10 +33,15 @@ for destdir in /home/*/Desktop/; do
     cp Firefox-ESR.desktop "$destdir" &&
     chown --reference="$destdir" "$destdir/Firefox-ESR.desktop"
 done
-echo -n;
+printf "\n";
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
 mkdir -p /etc/skel/Desktop ; cp Firefox-ESR.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
 rm FirefoxESR.tar.bz2 ; rm Firefox-ESR.desktop ;
-# Exit
+# Exit notice.
+# printf -- '%s\n' "" "" "" " Congratulations!" \
+#  " Mozilla Firefox Extended Support Release is now installed onto your computer." \
+#  " Mozilla Firefox Extended Support Release will update itself." \
+#  " Happy browsing." "" ""
+# exit
 exit 0
