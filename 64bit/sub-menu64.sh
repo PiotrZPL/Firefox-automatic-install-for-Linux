90d192fd1d6d1eed2d2966b5b84255fc17afa936
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-08-28 22:59:44 -0400

    excess line space fix
    The line spacing for the user notices was excessive. This change corrects that issue.

--- 64bit/sub-menu64.sh
+++ 64bit/sub-menu64.sh
@@ -7,7 +7,7 @@
 #
 while true :
 do
- printf -- '\n\n%s\n\n\n\n' " ";
+ printf -- '\n%s\n' " ";
  printf -- '%s\n' "   6 4 b i t - M E N U" \
  " " \
  " 1. Mozill Firefox" \
@@ -21,19 +21,19 @@ do
  printf " Please enter option [1 - 7]";
  read -r opt
  case $opt in
-  1) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Mozilla Firefox"; chmod +x ./64bit/FirefoxStable.sh; ./64bit/FirefoxStable.sh; break ;;
+  1) clear; printf -- '\n%s\n\n' " You selected Mozilla Firefox"; chmod +x ./64bit/FirefoxStable.sh; ./64bit/FirefoxStable.sh; break ;;
 
-  2) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Beta"; chmod +x ./64bit/FirefoxBeta.sh; ./64bit/FirefoxBeta.sh; break ;;
+  2) clear; printf -- '\n%s\n\n' " You selected Firefox Beta"; chmod +x ./64bit/FirefoxBeta.sh; ./64bit/FirefoxBeta.sh; break ;;
 
-  3) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Developer Edition"; chmod +x ./64bit/Firefox-Developer-Edition.sh; ./64bit/Firefox-Developer-Edition.sh; break ;;
+  3) clear; printf -- '\n%s\n\n' " You selected Firefox Developer Edition"; chmod +x ./64bit/Firefox-Developer-Edition.sh; ./64bit/Firefox-Developer-Edition.sh; break ;;
 
-  4) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Nightly"; chmod +x ./64bit/FirefoxNightly.sh; ./64bit/FirefoxNightly.sh; break ;;
+  4) clear; printf -- '\n%s\n\n' " You selected Firefox Nightly"; chmod +x ./64bit/FirefoxNightly.sh; ./64bit/FirefoxNightly.sh; break ;;
 
-  5) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected Firefox Extended Support Release"; chmod +x ./64bit/FirefoxESR.sh; ./64bit/FirefoxESR.sh; break ;;
+  5) clear; printf -- '\n%s\n\n' " You selected Firefox Extended Support Release"; chmod +x ./64bit/FirefoxESR.sh; ./64bit/FirefoxESR.sh; break ;;
 
-  6) clear; printf -- '\n\n\n%s\n\n\n\n' " You selected to install ALL 64-bit editions"; chmod +x ./64bit/firefox-all.sh; ./64bit/firefox-all.sh; break ;;
+  6) clear; printf -- '\n%s\n\n' " You selected to install ALL 64-bit editions"; chmod +x ./64bit/firefox-all.sh; ./64bit/firefox-all.sh; break ;;
 
-  7) clear; printf -- '\n\n\n%s\n\n\n\n' " Goodbye, $USER"; exit 1;;
+  7) clear; printf -- '\n%s\n\n' " Goodbye, $USER"; exit 1;;
 
   *) clear;
      printf -- '\n\n%s\n' " $opt is an invaild option. Please select option between 1-7 only" \
