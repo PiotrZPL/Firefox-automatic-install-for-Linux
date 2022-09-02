6a4c415477ee7c2afb303792cb9f1ca6c78cc6ba
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 04:09:20 -0400

    Update 32nightly-UNINSTALL.sh

--- 32bit/32bit-uninstallers/32nightly-UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32nightly-UNINSTALL.sh
@@ -3,7 +3,7 @@
 # Firefox Automatic Install for Linux -- Uninstall Firefox Nightly 32-bit -- File can be used independently
 #
 # Uninstalling Firefox notice
-printf -- '\n\n%s\n\n\n\n' " Uninstalling Firefox Nightly 32-bit release";
+printf -- '\n%s\n' " Uninstalling Firefox Nightly 32-bit release";
 # Small delay to give user time to read the above notice.
 sleep 3;
 # Installation
