45d4b81d8177289e73517f1c78e9036724aa17d8
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 19:52:13 -0400

    Update 32firefox_all_UNINSTALL.sh

--- 32bit/32bit_uninstallers/32firefox_all_UNINSTALL.sh
+++ 32bit/32bit_uninstallers/32firefox_all_UNINSTALL.sh
@@ -7,29 +7,29 @@ printf -- '\n%s\n' " Uninstalling ALL releases of Mozilla Firefox 32-bit";
 # Small delay to give user time to read the above notice.
 sleep 3;
 # Installation
-sudo rm -r -f /opt/firefox-32bit/ ;
-sudo rm -r -f /opt/firefox-beta-32bit/ ;
-sudo rm -r -f /opt/firefox-developer-edition-32bit/ ;
-sudo rm -r -f /opt/firefox-nightly-32bit/ ;
-sudo rm -r -f /opt/firefox-esr-32bit/ ;
+sudo rm -r -f /opt/firefox_32bit/ ;
+sudo rm -r -f /opt/firefox_beta_32bit/ ;
+sudo rm -r -f /opt/firefox_developer_edition_32bit/ ;
+sudo rm -r -f /opt/firefox_nightly_32bit/ ;
+sudo rm -r -f /opt/firefox_esr_32bit/ ;
 # Menu shotcuts
-sudo rm -r -f /usr/share/applications/Firefox-32bit.desktop ;
-sudo rm -r -f /usr/share/applications/Firefox-Beta-32bit.desktop ;
-sudo rm -r -f /usr/share/applications/Firefox-Developer-Edition-32bit.desktop ;
-sudo rm -r -f /usr/share/applications/Firefox-Nightly-32bit.desktop ;
-sudo rm -r -f /usr/share/applications/Firefox-ESR-32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox_32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox_Beta_32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox_Developer_Edition_32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox_Nightly_32bit.desktop ;
+sudo rm -r -f /usr/share/applications/Firefox_ESR_32bit.desktop ;
 # Desktop shortcuts
-sudo rm -r -f /etc/skel/Desktop/Firefox-32bit.desktop ;
-sudo rm -r -f /etc/skel/Desktop/Firefox-Beta-32bit.desktop ;
-sudo rm -r -f /etc/skel/Desktop/Firefox-Developer-Edition-32bit.desktop ;
-sudo rm -r -f /etc/skel/Desktop/Firefox-Nightly-32bit.desktop ;
-sudo rm -r -f /etc/skel/Desktop/Firefox-ESR-32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox_32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox_Beta_32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox_Developer_Edition_32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox_Nightly_32bit.desktop ;
+sudo rm -r -f /etc/skel/Desktop/Firefox_ESR_32bit.desktop ;
 # Current deskop shortcuts
-sudo rm -r -f /home/*/Desktop/Firefox-32bit.desktop ;
-sudo rm -r -f /home/*/Desktop/Firefox-Beta-32bit.desktop ;
-sudo rm -r -f /home/*/Desktop/Firefox-Developer-Edition-32bit.desktop ;
-sudo rm -r -f /home/*/Desktop/Firefox-Nightly-32bit.desktop ;
-sudo rm -r -f /home/*/Desktop/Firefox-ESR-32bit.desktop ;
+sudo rm -r -f /home/*/Desktop/Firefox_32bit.desktop ;
+sudo rm -r -f /home/*/Desktop/Firefox_Beta_32bit.desktop ;
+sudo rm -r -f /home/*/Desktop/Firefox_Developer_Edition_32bit.desktop ;
+sudo rm -r -f /home/*/Desktop/Firefox_Nightly_32bit.desktop ;
+sudo rm -r -f /home/*/Desktop/Firefox_ESR_32bit.desktop ;
 # Uncommend to enable PURGE - Cache files (all users).
 #sudo rm -r -f /home/*/.cache/mozilla/firefox/ ;
 # Uncommend to enable PUEGE - Configuration and profile files (all users).
