5b7d97cbe1d8a3dc758ba55680c7a414b54665c2
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 04:13:54 -0400

    Update sub-menu32.sh

--- 32bit/sub-menu32.sh
+++ 32bit/sub-menu32.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Firefox automatic install for Linux
-#   v2.9.0.3
+#   v2.9.0.4
 #
 # -- This file to be used with Setup.sh
 #
@@ -21,17 +21,17 @@ do
  printf " Please enter option [1 - 7]";
  read -r opt
  case $opt in
-  1) clear; printf -- '\n%s\n\n' " You selected Mozilla Firefox"; chmod +x ./32bit/FirefoxStable32.sh; ./32bit/FirefoxStable32.sh; break ;;
+  1) clear; printf -- '\n%s\n\n' " You selected Mozilla Firefox"; chmod +x ./32bit/Firefox_Stable32.sh; ./32bit/Firefox_Stable32.sh; break ;;
 
-  2) clear; printf -- '\n%s\n\n' " You selected Firefox Beta"; chmod +x ./32bit/FirefoxBeta32.sh; ./32bit/FirefoxBeta32.sh; break ;;
+  2) clear; printf -- '\n%s\n\n' " You selected Firefox Beta"; chmod +x ./32bit/Firefox_Beta32.sh; ./32bit/Firefox_Beta32.sh; break ;;
 
-  3) clear; printf -- '\n%s\n\n' " You selected Firefox Developer Edition"; chmod +x ./32bit/Firefox-Developer-Edition32.sh; ./32bit/Firefox-Developer-Edition32.sh; break ;;
+  3) clear; printf -- '\n%s\n\n' " You selected Firefox Developer Edition"; chmod +x ./32bit/Firefox_Developer_Edition32.sh; ./32bit/Firefox_Developer_Edition32.sh; break ;;
 
-  4) clear; printf -- '\n%s\n\n' " You selected Firefox Nightly"; chmod +x ./32bit/FirefoxNightly32.sh; ./32bit/FirefoxNightly32.sh; break ;;
+  4) clear; printf -- '\n%s\n\n' " You selected Firefox Nightly"; chmod +x ./32bit/Firefox_Nightly32.sh; ./32bit/Firefox_Nightly32.sh; break ;;
 
-  5) clear; printf -- '\n%s\n\n' " You selected Firefox Extended Support Release"; chmod +x ./32bit/FirefoxESR32.sh; ./32bit/FirefoxESR32.sh; break ;;
+  5) clear; printf -- '\n%s\n\n' " You selected Firefox Extended Support Release"; chmod +x ./32bit/Firefox_ESR32.sh; ./32bit/Firefox_ESR32.sh; break ;;
 
-  6) clear; printf -- '\n%s\n\n' " You selected to install ALL 32-bit editions"; chmod +x ./32bit/firefox-all32.sh; ./32bit/firefox-all32.sh; break ;;
+  6) clear; printf -- '\n%s\n\n' " You selected to install ALL 32-bit editions"; chmod +x ./32bit/firefox_all32.sh; ./32bit/firefox_all32.sh; break ;;
 
   7) clear; printf -- '\n%s\n\n' " Goodbye, $USER"; exit 1;;
 
