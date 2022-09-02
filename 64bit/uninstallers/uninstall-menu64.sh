68a2973bfb14ec99d5292942afe66aab3aac6b28
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 01:46:40 -0400

    Update uninstall-menu64.sh

--- 64bit/uninstallers/uninstall-menu64.sh
+++ 64bit/uninstallers/uninstall-menu64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Firefox automatic install for Linux
-#   v2.9.0.3
+#   v2.9.0.4
 #
 # -- This file to be used with Setup.sh
 #
@@ -28,21 +28,21 @@ printf -- '%s\n' "   U N I N S T A L L - 6 4 b i t - M E N U" \
  printf " Please enter option [1 - 7]";
  read -r opt
  case $opt in
-  1) clear; printf -- '\n%s\n\n' " You selected Mozilla Firefox"; chmod +x ./64bit/uninstallers/firefox-UNINSTALL.sh; ./64bit/uninstallers/firefox-UNINSTALL.sh; break ;;
+  1) clear; printf -- '\n%s\n\n' " You selected Mozilla Firefox"; chmod +x ./64bit/uninstallers/firefox_UNINSTALL.sh; ./64bit/uninstallers/firefox_UNINSTALL.sh; break ;;
 
-  2) clear; printf -- '\n%s\n\n' " You selected Firefox Beta"; chmod +x ./64bit/uninstallers/beta-UNINSTALL.sh; ./64bit/uninstallers/beta-UNINSTALL.sh; break ;;
+  2) clear; printf -- '\n%s\n\n' " You selected Firefox Beta"; chmod +x ./64bit/uninstallers/beta_UNINSTALL.sh; ./64bit/uninstallers/beta_UNINSTALL.sh; break ;;
 
-  3) clear; printf -- '\n%s\n\n' " You selected Firefox Developer Edition"; chmod +x ./64bit/uninstallers/firefox-dev-UNINSTALL.sh; ./64bit/uninstallers/firefox-dev-UNINSTALL.sh; break ;;
+  3) clear; printf -- '\n%s\n\n' " You selected Firefox Developer Edition"; chmod +x ./64bit/uninstallers/firefox_dev_UNINSTALL.sh; ./64bit/uninstallers/firefox_dev_UNINSTALL.sh; break ;;
 
-  4) clear; printf -- '\n%s\n\n' " You selected Firefox Nightly"; chmod +x ./64bit/uninstallers/nightly-UNINSTALL.sh; ./64bit/uninstallers/nightly-UNINSTALL.sh; break ;;
+  4) clear; printf -- '\n%s\n\n' " You selected Firefox Nightly"; chmod +x ./64bit/uninstallers/nightly_UNINSTALL.sh; ./64bit/uninstallers/nightly_UNINSTALL.sh; break ;;
 
-  5) clear; printf -- '\n%s\n\n' " You selected Firefox Extended Support Release"; chmod +x ./64bit/uninstallers/esr-UNINSTALL.sh; ./64bit/uninstallers/esr-UNINSTALL.sh; break ;;
+  5) clear; printf -- '\n%s\n\n' " You selected Firefox Extended Support Release"; chmod +x ./64bit/uninstallers/esr_UNINSTALL.sh; ./64bit/uninstallers/esr_UNINSTALL.sh; break ;;
 
-  6) clear; printf -- '\n%s\n\n' " You selected to remove ALL 64-bit editions"; chmod +x ./64bit/uninstallers/firefox-all-UNINSTALL.sh; ./64bit/uninstallers/firefox-all-UNINSTALL.sh; break ;;
+  6) clear; printf -- '\n%s\n\n' " You selected to remove ALL 64-bit editions"; chmod +x ./64bit/uninstallers/firefox_all_UNINSTALL.sh; ./64bit/uninstallers/firefox_all_UNINSTALL.sh; break ;;
 
   7) clear; printf -- '\n%s\n\n' " Goodbye, $USER"; exit 1;;
   
-  66) clear; printf -- '\n%s\n\n' " Per your request, $USER. Execute Order 66."; chmod +x ./64bit/uninstallers/PURGE-64.sh; ./64bit/uninstallers/PURGE-64.sh; break ;;
+  66) clear; printf -- '\n%s\n\n' " Per your request, $USER. Execute Order 66."; chmod +x ./64bit/uninstallers/PURGE_64.sh; ./64bit/uninstallers/PURGE_64.sh; break ;;
 
   *) clear;
      printf -- '\n\n%s\n' " $opt is an invaild option. Please select option between 1-7 only" \
