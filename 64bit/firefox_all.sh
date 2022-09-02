a8f9b4e0f6dd7b785a4edc45ff2a609b221227c4
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 16:52:04 -0400

    Update and rename firefox-all.sh to firefox_all.sh

--- 64bit/firefox_all.sh
+++ 64bit/firefox_all.sh
@@ -11,15 +11,15 @@ sleep 2;
 # Visual spacing
 printf -- '\n\n\n%s\n\n' " ";
 # Firefox Stable
-chmod +x ./64bit/FirefoxStable.sh; ./64bit/FirefoxStable.sh;
+chmod +x ./64bit/Firefox_Stable.sh; ./64bit/Firefox_Stable.sh;
 # Firefox Beta
-chmod +x ./64bit/FirefoxBeta.sh; ./64bit/FirefoxBeta.sh;
+chmod +x ./64bit/Firefox_Beta.sh; ./64bit/Firefox_Beta.sh;
 # Firefox Developer Edition
-chmod +x ./64bit/Firefox-Developer-Edition.sh; ./64bit/Firefox-Developer-Edition.sh;
+chmod +x ./64bit/Firefox_Developer_Edition.sh; ./64bit/Firefox_Developer_Edition.sh;
 # Firefox Nightly
-chmod +x ./64bit/FirefoxNightly.sh; ./64bit/FirefoxNightly.sh;
+chmod +x ./64bit/Firefox_Nightly.sh; ./64bit/Firefox_Nightly.sh;
 # Firefox Extended Support Release
-chmod +x ./64bit/FirefoxESR.sh; ./64bit/FirefoxESR.sh;
+chmod +x ./64bit/Firefox_ESR.sh; ./64bit/Firefox_ESR.sh;
 # Exit notice
 printf -- '%s\n' "" "" "" " Congratulations!" \
   " ALL editions of Mozilla Firfox have been installed onto your computer." \
