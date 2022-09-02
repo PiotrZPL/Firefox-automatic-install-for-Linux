c8e9bf3783472a02e7fe5f88a1202b577d9965c2
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 04:05:31 -0400

    Update 32beta_UNINSTALL.sh

--- 32bit/32bit-uninstallers/32beta_UNINSTALL.sh
+++ 32bit/32bit-uninstallers/32beta_UNINSTALL.sh
@@ -3,7 +3,7 @@
 # Firefox Automatic Install for Linux -- Uninstall Firefox Beta 32-bit -- File can be used independently
 #
 # Uninstalling Firefox notice
-printf -- '\n\n%s\n\n\n\n' " Uninstalling Mozilla Firefox Beta 32-bit ";
+printf -- '\n%s\n' " Uninstalling Mozilla Firefox Beta 32-bit ";
 # Small delay to give user time to read the above notice.
 sleep 3;
 # Installation
