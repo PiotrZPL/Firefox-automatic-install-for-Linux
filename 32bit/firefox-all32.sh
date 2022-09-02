95084e9b511fa9e00e2c36810f5efd9e8128ab99
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 04:20:28 -0400

    Update firefox-all32.sh

--- 32bit/firefox-all32.sh
+++ 32bit/firefox-all32.sh
@@ -3,23 +3,23 @@
 # This will install ALL 32-bit releases. To be used with Setup.sh
 #
 # Firefox automatic install for Linux - Universal shell edition
-# v2.9.0.3
+# v2.9.0.4
 #
-printf -- '\n\n%s\n\n\n\n' " Now installing ALL 32-bt editions of Mozilla Firefox. Please wait... ";
+printf -- '\n%s\n' " Now installing ALL 32-bt editions of Mozilla Firefox. Please wait... ";
 # Give time for user to read notice.
 sleep 2; 
 # Visual spacing
 printf -- '\n\n\n%s\n\n' " ";
 # Firefox Stable
-chmod +x ./32bit/FirefoxStable32.sh; ./32bit/FirefoxStable32.sh; 
+chmod +x ./32bit/Firefox_Stable32.sh; ./32bit/Firefox_Stable32.sh; 
 # Firefox Beta
-chmod +x ./32bit/FirefoxBeta32.sh; ./32bit/FirefoxBeta32.sh;
+chmod +x ./32bit/Firefox_Beta32.sh; ./32bit/Firefox_Beta32.sh;
 # Firefox Developer
-chmod +x ./32bit/Firefox-Developer-Edition32.sh; ./32bit/Firefox-Developer-Edition32.sh; 
+chmod +x ./32bit/Firefox_Developer_Edition32.sh; ./32bit/Firefox_Developer_Edition32.sh; 
 # Firefox Nightly
-chmod +x ./32bit/FirefoxNightly32.sh; ./32bit/FirefoxNightly32.sh; 
+chmod +x ./32bit/Firefox_Nightly32.sh; ./32bit/Firefox_Nightly32.sh; 
 # Firefox Extended Support Release
-chmod +x ./32bit/FirefoxESR32.sh; ./32bit/FirefoxESR32.sh;
+chmod +x ./32bit/Firefox_ESR32.sh; ./32bit/Firefox_ESR32.sh;
 # Exit notice
 printf -- '%s\n' "" "" "" " Congratulations!" \
   " ALL editions of Mozilla Firfox 32-bit have been installed onto your computer." \
