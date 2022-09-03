5d28e6e4b133aaf80db25a7d8252a1147d496dc7
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-02 21:05:27 -0400

    Update firefox_developer.sh

--- oem/firefox_developer.sh
+++ oem/firefox_developer.sh
@@ -1,47 +1,36 @@
 #!/bin/sh
 #
-# Firefox Developer Edition - oem - silent install
+# Firefox Automatic Install for Linux - Firefox Developer Edition - oem - silent install - To be used with error.sh
 #
 # Make error.sh exactable so it can execut if needed.
 chmod +x ./error.sh;
-# Download notice.
-# printf -- '\n\n%s\n\n\n\n' " Please wait. I am downloading the latest version of Mozilla Firefox Developer Edition.";
-#4-second wait before beginning download. Gives user time to read the above sentence and understand what is happening.
-# sleep 4;
 # Download using wget with curl failback.
 wget -L -O "FirefoxDeveloperEdition.tar.bz2" "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64" >/dev/null || curl -L -o "FirefoxDeveloperEdition.tar.bz2" "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64" || /.error.sh ;
-# Begin install notice.
-# printf -- '\n\n\n%s\n\n' " Installing Mozilla Firefox Developer Edition.";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 mkdir -p -m 755 /opt ;
 # Path where to be installed.
-mkdir /opt/firefox-developer-edition ;
+mkdir /opt/firefox_developer_edition ;
 # Extracts to install path
-tar xjf FirefoxDeveloperEdition.tar.bz2 -C /opt/firefox-developer-edition/ ;
+tar xjf FirefoxDeveloperEdition.tar.bz2 -C /opt/firefox_developer_edition/ ;
 # Required permissions needed for Mozilla Firefox automatic update feature to work.
-chmod -R 757 /opt/firefox-developer-edition/firefox/ ;
+chmod -R 757 /opt/firefox_developer_edition/firefox/ ;
 # Start create icon script.
 chmod +x ./icon_developer64.sh ; bash ./icon_developer64.sh ;
 # Give time for icon script to complete.
 sleep 2;
 # Makes icon executable allowing it to run Firefox (which is also executable).
-chmod +x Firefox-Developer-Edition.desktop ;
+chmod +x Firefox_Developer_Edition.desktop ;
 # Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...).
-cp Firefox-Developer-Edition.desktop /usr/share/applications ;
+cp Firefox_Developer_Edition.desktop /usr/share/applications ;
 # Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all).
 for destdir in /home/*/Desktop/; do
-    cp Firefox-Developer-Edition.desktop "$destdir" &&
-    chown --reference="$destdir" "$destdir/Firefox-Developer-Edition.desktop"
+    cp Firefox_Developer_Edition.desktop "$destdir" &&
+    chown --reference="$destdir" "$destdir/Firefox_Developer_Edition.desktop"
 done
 printf "\n";
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
-mkdir -p /etc/skel/Desktop ; cp Firefox-Developer-Edition.desktop /etc/skel/Desktop ;
+mkdir -p /etc/skel/Desktop ; cp Firefox_Developer_Edition.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
-rm FirefoxDeveloperEdition.tar.bz2 ; rm Firefox-Developer-Edition.desktop ;
-# Exit notice.
-# printf -- '%s\n' "" "" "" " Congratulations!" \
-#  " Mozilla Firefox Developer Edition is now installed onto your computer." \
-#  " Mozilla Firefox Developer Edition will update itself." \
-#  " Happy browsing." "" ""
+rm FirefoxDeveloperEdition.tar.bz2 ; rm Firefox_Developer_Edition.desktop ;
 # exit
 exit 0
