e48fb8b0e312243cef7442590fb4442af2b874f7
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-04 16:34:40 -0400

    GitHub ERRROR
    So for unknown reasons, GitHub rolled back my work. -- This will be the 2nd time GitHub has proven to be problematic. The 1st time was over a year ago and you can read about it here: https://github.com/Linux-Is-Best/Firefox-automatic-install-for-Linux/issues/3

--- 64bit/Firefox_Nightly.sh
+++ 64bit/Firefox_Nightly.sh
@@ -15,25 +15,25 @@ printf -- '\n\n\n%s\n\n' " Installing Mozilla Firefox Nightly.";
 # Checks if distro has default opt path and if not found adds opt with default permissions.
 sudo mkdir -p -m 755 /opt ;
 # Path where to be installed.
-sudo mkdir /opt/firefox-nightly ;
+sudo mkdir /opt/firefox_nightly ;
 # Extracts to install path.
-sudo tar xjf FirefoxNightly.tar.bz2 -C /opt/firefox-nightly/ ;
+sudo tar xjf FirefoxNightly.tar.bz2 -C /opt/firefox_nightly/ ;
 # Required permissions needed for Mozilla Firefox automatic update feature to work.
-sudo chmod -R 757 /opt/firefox-nightly/firefox/ ;
+sudo chmod -R 757 /opt/firefox_nightly/firefox/ ;
 # Start create icon script.
 chmod +x ./64bit/icon_nightly64.sh ; bash ./64bit/icon_nightly64.sh ;
 # Give time for icon script to complete.
 sleep 2;
 # Makes icon executable allowing it to run Firefox (which is also executable).
-chmod +x Firefox-Nightly.desktop ;
+chmod +x Firefox_Nightly.desktop ;
 # Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...).
-sudo cp Firefox-Nightly.desktop /usr/share/applications ;
+sudo cp Firefox_Nightly.desktop /usr/share/applications ;
 # Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all).
 sudo find /home/*/Desktop -maxdepth 1 -type d -exec cp Firefox_Nightly.desktop '{}' \; -exec chown --reference='{}' '{}/Firefox_Nightly.desktop' \;
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
-sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox-Nightly.desktop /etc/skel/Desktop ;
+sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox_Nightly.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
-rm FirefoxNightly.tar.bz2 ; rm Firefox-Nightly.desktop ;
+rm FirefoxNightly.tar.bz2 ; rm Firefox_Nightly.desktop ;
 # Exit notice.
 printf -- '%s\n' "" "" "" " Congratulations!" \
   " Mozilla Firefox Nightly is now installed onto your computer." \
