f396d93e0bc1eb51a2feb59e110532175c611fd6
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-04 03:15:16 -0400

    Update Firefox_ESR.sh

--- 64bit/Firefox_ESR.sh
+++ 64bit/Firefox_ESR.sh
@@ -29,11 +29,7 @@ chmod +x Firefox-ESR.desktop ;
 # Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...).
 sudo cp Firefox-ESR.desktop /usr/share/applications ;
 # Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all).
-for destdir in /home/*/Desktop/; do
-    cp Firefox-ESR.desktop "$destdir" &&
-    chown --reference="$destdir" "$destdir/Firefox-ESR.desktop"
-done
-printf "\n";
+sudo find /home/*/Desktop -maxdepth 1 -type d -exec cp Firefox_ESR.desktop '{}' \; -exec chown --reference='{}' '{}/Firefox_ESR.desktop' \;
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
 sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox-ESR.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
