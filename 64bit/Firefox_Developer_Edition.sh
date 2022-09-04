16bdbf7bd7bc7ef9e257617442964957c4b386eb
Linux-Is-Best <61557098+Linux-Is-Best@users.noreply.github.com>
2022-09-04 03:14:17 -0400

    Update Firefox_Developer_Edition.sh

--- 64bit/Firefox_Developer_Edition.sh
+++ 64bit/Firefox_Developer_Edition.sh
@@ -29,11 +29,7 @@ chmod +x Firefox_Developer_Edition.desktop ;
 # Adds icon to application menu (xfce, gnome, cinnamon, mate, deepin, etc...).
 sudo cp Firefox_Developer_Edition.desktop /usr/share/applications ;
 # Copies desktop icon to all user desktops and grants them ownership (it is their desktop after all).
-for destdir in /home/*/Desktop/; do
-    cp Firefox_Developer_Edition.desktop "$destdir" &&
-    chown --reference="$destdir" "$destdir/Firefox_Developer_Edition.desktop"
-done
-printf "\n";
+sudo find /home/*/Desktop -maxdepth 1 -type d -exec cp Firefox_Developer_Edition.desktop '{}' \; -exec chown --reference='{}' '{}/Firefox_Developer_Edition.desktop' \;
 # Adds a desktop icon to all FUTURE new login users (assuming you make any).
 sudo mkdir -p /etc/skel/Desktop ; sudo cp Firefox_Developer_Edition.desktop /etc/skel/Desktop ;
 # Removes the temporary files no longer needed.
